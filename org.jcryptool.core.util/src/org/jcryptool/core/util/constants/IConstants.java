041a5e147dc2092c5ea9c751e2fa390ca2ffa4cd
Dominik Schadow <dominikschadow@googlemail.com>
2011-09-01 09:52:33 +0200

    Added description on how to use the org.jcryptool.core.util plug-in and its provided services/interfaces
    Signed-off-by: Dominik Schadow <dominikschadow@googlemail.com>

--- org.jcryptool.core.util/src/org/jcryptool/core/util/constants/IConstants.java
+++ org.jcryptool.core.util/src/org/jcryptool/core/util/constants/IConstants.java
@@ -10,18 +10,26 @@
 package org.jcryptool.core.util.constants;
 
 /**
- * Interface for JCrypTool global constants.
+ * Interface for JCrypTool global constants. Use this constants in your plug-in whereever possible.
  *
  * @author Dominik Schadow
  * @version 0.9.5
  */
 public interface IConstants {
+	/** Constant file extension for <b>binary</b> files (value is {@value}). */
     String BIN_FILE_TYPE_EXTENSION = ".bin"; //$NON-NLS-1$
+	/** Constant file extension for <b>text</b> files (value is {@value}). */
     String TXT_FILE_TYPE_EXTENSION = ".txt"; //$NON-NLS-1$
+	/** Constant file extension for <b>xml</b> files (value is {@value}). */
     String XML_FILE_TYPE_EXTENSION = ".xml"; //$NON-NLS-1$
+	/** Constant regular expression for generated <b>output files</b> (value is {@value}). */
     String OUTPUT_REGEXP = "out\\d\\d\\d.((xml)|(bin)|(txt))"; //$NON-NLS-1$
+	/** Constant filter extension for <b>text</b> files (value is {@value}). Use in file dialogs. */
     String TXT_FILTER_EXTENSION = "*.txt"; //$NON-NLS-1$
+	/** Constant filter extension for <b>all</b> files (value is {@value}). Use in file dialogs. */
     String ALL_FILTER_EXTENSION = "*.*"; //$NON-NLS-1$
+	/** Constant internationalized filter name for <b>text</b> files. Use in file dialogs. */
     String TXT_FILTER_NAME = Messages.IConstants_0;
+	/** Constant internationalized filter name for <b>all</b> files. Use in file dialogs. */
     String ALL_FILTER_NAME = Messages.IConstants_1;
 }
