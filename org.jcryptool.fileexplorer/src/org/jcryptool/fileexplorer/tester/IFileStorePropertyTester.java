e5ed9b60eefb23c5c527bf7e11477a2c3dfced30
Dominik Schadow <dominikschadow@googlemail.com>
2011-09-06 15:38:06 +0200

    Removed duplicated import action cascade handler
    Duplicated import action cascade handler deactivated all property checks
    on command --> removed!
    
    Signed-off-by: Dominik Schadow <dominikschadow@googlemail.com>

--- org.jcryptool.fileexplorer/src/org/jcryptool/fileexplorer/tester/IFileStorePropertyTester.java
+++ org.jcryptool.fileexplorer/src/org/jcryptool/fileexplorer/tester/IFileStorePropertyTester.java
@@ -1,7 +1,7 @@
 // -----BEGIN DISCLAIMER-----
 /*******************************************************************************
- * Copyright (c) 2010 JCrypTool Team and Contributors
- * 
+ * Copyright (c) 2011 JCrypTool Team and Contributors
+ *
  * All rights reserved. This program and the accompanying materials are made available under the terms of the Eclipse
  * Public License v1.0 which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
@@ -18,27 +18,26 @@ import org.jcryptool.core.logging.utils.LogUtil;
  * <p>
  * Tester for a IFileStore to check whether the file extension is of the expected type.
  * </p>
- * 
+ *
  * <ul>
  * <li><b>isCascade:</b> The expected value may contain a dot before the extension (like <b>.xml</b>) or not, but it
  * must not contain a * (like <b>*.xml</b>).</li>
  * <li><b>isFile:</b> The expected value must be empty.</li>
  * <li><b>isRoot:</b> The expected value must be empty.</li>
  * </ul>
- * 
+ *
  * @author Dominik Schadow
- * @version 0.9.3
+ * @version 0.9.5
  */
 public class IFileStorePropertyTester extends PropertyTester {
     public boolean test(Object receiver, String property, Object[] args, Object expectedValue) {
         boolean result = false;
-
         try {
             if ("isCascade".equals(property)) {
                 result = ((IFileStore) receiver).getName().endsWith(expectedValue.toString());
             } else if ("isFile".equals(property)) {
                 result = !((IFileStore) receiver).fetchInfo().isDirectory();
-                if (result) // for empty folders fetchInfo().isDirectory() = false, so this second test is neccessary
+                if (result) // for empty folders fetchInfo().isDirectory() = false, so this second test is necessary
                 {
                     result = !((IFileStore) receiver).toLocalFile(EFS.NONE, null).isDirectory();
                 }
