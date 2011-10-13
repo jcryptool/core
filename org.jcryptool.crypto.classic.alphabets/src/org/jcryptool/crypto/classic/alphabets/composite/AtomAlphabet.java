6b3b832ba092b741610627128abeca709224f33b
Simon_Leischnig <simonjena@gmail.com>
2011-10-13 16:54:34 +0200

    composite alphabet tools

---
+++ org.jcryptool.crypto.classic.alphabets/src/org/jcryptool/crypto/classic/alphabets/composite/AtomAlphabet.java
@@ -0,0 +1,184 @@
+package org.jcryptool.crypto.classic.alphabets.composite;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.jcryptool.core.operations.alphabets.AbstractAlphabet;
+
+//TODO: change method names to sth more suitable (toStringShort....)
+/**
+ * Basic alphabet which may be created solely from its characters
+ * 
+ * @author Simon L
+ *
+ */
+public class AtomAlphabet extends AbstractAlphabet {
+	
+	public static final String ATOM_ALPHABET_NAME = "AtomAlphabet";
+	
+	public static final boolean IS_STANDARD_ALPHABET_STD = false;
+	public static final boolean IS_BASIC_STD = false;
+	public static final String NAME_STD = null;
+	public static final String SHORTNAME_STD = null;
+	
+	private List<Character> content;
+	private boolean isBasic = IS_BASIC_STD;
+	private boolean isDefault = IS_STANDARD_ALPHABET_STD;
+	/**
+	 * null => generated name is returned in getter
+	 */
+	private String name;
+	/**
+	 * null => generated short name is returned in getter
+	 */
+	private String shortName;
+	
+	/**
+	 * @param characters
+	 * @param isBasic
+	 * @param isDefault
+	 * @param name
+	 * @param shortName
+	 * 
+	 * @deprecated the additional parameters will soon be managed in the alphabets manager
+	 */
+	public AtomAlphabet(List<Character> characters, boolean isBasic, boolean isDefault, String name, String shortName) {
+		this.content = characters;
+		setName(name);
+		setShortName(shortName);
+		setBasic(isBasic);
+		setDefaultAlphabet(isDefault);
+	}
+
+	/**
+	 * creates an alphabet solely from its containing characters, defaulting the other parameters
+	 * (see public static standard fields like {@value AtomAlphabet#IS_STANDARD_ALPHABET_STD}.
+	 * <br />
+	 * Double characters will be ignored.<br />
+	 * 
+	 * @param characters the characters of the alphabet
+	 */
+	public AtomAlphabet(List<Character> characters) {
+		this.content = filterDoublets(characters);
+	}
+	
+
+	/**
+	 * creates an alphabet solely from its containing characters, defaulting the other parameters
+	 * (see public static standard fields like {@value AtomAlphabet#IS_STANDARD_ALPHABET_STD}.
+	 * <br />
+	 * by default, the names are generated from the alphabet content.
+	 * 
+	 * @param characters the characters of the alphabet
+	 */
+	public AtomAlphabet(String characters) {
+		this(StringAlphabetFactory.stringToList(characters));
+	}
+	
+	/**
+	 * creates an alphabet solely from its containing characters, defaulting the other parameters
+	 * (see public static standard fields like {@value AtomAlphabet#IS_STANDARD_ALPHABET_STD}.
+	 * <br />
+	 * by default, the names are generated from the alphabet content.
+	 * 
+	 * @param characters the characters of the alphabet
+	 */
+	public AtomAlphabet(char[] characters) {
+		this(StringAlphabetFactory.stringToList(characters));
+	}
+
+	private static List<Character> filterDoublets(List<Character> characters) {
+		List<Character> ref = new LinkedList<Character>();
+		for(Character c: characters) {
+			if(! ref.contains(c)) {
+				ref.add(c);
+			}
+		}
+		
+		return ref;
+	}
+
+	public char[] getCharacterSet() {
+		return StringAlphabetFactory.toCharArray(content);
+	}
+	public boolean contains(char e) {
+		return content.contains(e);
+	}
+	
+	@Override
+	public String toString() {
+		return StringAlphabetFactory.buildStringRepresentation(this);
+	}
+
+	@Override
+	public boolean isDefaultAlphabet() {
+		return isDefault;
+	}
+
+	@Override
+	public int getDisplayMissingCharacters() {
+		// wont be available for much longer
+		return -1;
+	}
+
+	@Override
+	public char getSubstituteCharacter() {
+		// wont be available for much longer
+		return 'X';
+	}
+
+	@Override
+	public void setDefaultAlphabet(boolean b) {
+		this.isDefault = b;
+	}
+
+	@Override
+	public void setCharacterSet(char[] characterSet) {
+		this.content = StringAlphabetFactory.stringToList(characterSet);
+	}
+
+	@Override
+	public boolean isBasic() {
+		return isBasic;
+	}
+
+	@Override
+	public void setBasic(boolean basic) {
+		this.isBasic = basic;
+	}
+
+	@Override
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public void setShortName(String shortName) {
+		this.shortName = shortName;
+	}
+
+	public String getShortName() {return shortName != null ? shortName : generateShortName();} //$NON-NLS-1$
+
+	public String getName() {return name != null ? name : generateName();} //$NON-NLS-1$
+
+	/**
+	 * Method for creating a generated short name for the alphabet if no short name is explicitely set
+	 * 
+	 * @return a generated name
+	 * @deprecated like {@link #getShortName()}
+	 */
+	private String generateShortName() {
+		return generateName();
+	}
+
+	/**
+	 * Method for creating a generated name for the alphabet if no short name is explicitely set
+	 * 
+	 * @return a generated name
+	 * @deprecated like {@link #getName()}
+	 */
+	private String generateName() {
+		return toString();
+	}
+	
+}
---
+++ org.jcryptool.crypto.classic.alphabets/src/org/jcryptool/crypto/classic/alphabets/composite/StringAlphabetFactory.java
@@ -0,0 +1,187 @@
+package org.jcryptool.crypto.classic.alphabets.composite;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Responsible for building String representations of AtomAlphabet objects, and parsing them back.
+ * 
+ * @author Simon L
+ *
+ */
+public class StringAlphabetFactory {
+
+	public static final String TO_STRING_LEVEL_DOWN = "((";
+	public static final String TO_STRING_LEVEL_UP = "))";
+	public static final String TO_STRING_SEPARATOR = "||";
+	public static final String TO_SHORT_STRING_LEVEL_DOWN = "(";
+	public static final String TO_SHORT_STRING_LEVEL_UP = ")";
+	public static final String TO_SHORT_STRING_SEPARATOR = "|";
+	public static final String ALPHABET_TYPE_SEPARATOR = "=";
+
+	public static final String ID_ATOM_ALPHA = "AtomAlphabet";
+	public static final String ID_EXCLUDE_CHAR_ALPHA_ID = "ExcludeCharAlphabet";
+	public static final String ID_COMPOSITE_ALPHA_ID = "CompositeAlphabet";
+	
+	public static String buildStringRepresentation(AtomAlphabet alphabet) {
+		if(alphabet instanceof CompositeAlphabet) {
+			CompositeAlphabet compositeAlphabet = (CompositeAlphabet) alphabet;
+			return buildStringRepresentationCompositeAlphabet(compositeAlphabet);
+		} else if (alphabet instanceof ExcludeCharAlphabet) {
+			ExcludeCharAlphabet excludeCharAlphabet = (ExcludeCharAlphabet) alphabet;
+			return buildStringRepresentationExcludeCharAlphabet(excludeCharAlphabet);
+		}
+		
+		//TODO: !provisory char[] -> List<Character> "stringToList" 
+		return ID_ATOM_ALPHA + StringAlphabetFactory.ALPHABET_TYPE_SEPARATOR + listToString(stringToList(alphabet.getCharacterSet()));
+	}
+
+	private static String buildStringRepresentationExcludeCharAlphabet(
+			ExcludeCharAlphabet excludeCharAlphabet) {
+		return buildAlphabetRepresentationByArguments(ID_EXCLUDE_CHAR_ALPHA_ID, 
+				buildStringRepresentation(excludeCharAlphabet),
+				excludeCharAlphabet.getExcludedChar().toString());
+	}
+
+	private static String buildStringRepresentationCompositeAlphabet(
+			CompositeAlphabet compositeAlphabet) {
+		//arguments: residualAlphabets string representation 
+		List<AtomAlphabet> residualAlphabets = compositeAlphabet.getResidualAlphabets();
+		String[] argList = new String[residualAlphabets.size()];
+		
+		for (int i = 0; i < residualAlphabets.size(); i++) {
+			argList[i] = buildStringRepresentation(residualAlphabets.get(i));
+		}
+		
+		return buildAlphabetRepresentationByArguments(ID_COMPOSITE_ALPHA_ID, argList);
+	}
+
+	/**
+	 * Returns the position of a specified string on the zero level in a String beginning at a specific index. 
+	 * 
+	 * @param string the string to search in
+	 * @param beginIndex the first index to search for separators
+	 * @param levelDownOp 
+	 * @return the index of the first character of the first encountered separator, or -1, if there is 
+	 * no separator found. (only separators on level zero are "found"). If the position is greater than 
+	 * or equal to the length of the String, -1 is returned.
+	 */
+	private static int findStringPositionOnLevelZero(String searchField, String searchSubject, String levelDownOp, String levelUpOp, int beginIndex) {
+		int index = beginIndex;
+		int level = 0;
+		
+		while(index < searchField.length()) {
+			int nextSep = searchField.indexOf(searchSubject, index);
+			if(nextSep == -1) return -1;
+			
+			int nextLvlDown = searchField.indexOf(levelDownOp, index);
+			int nextLvlUp = searchField.indexOf(levelUpOp, index);
+			
+			if(level <= 0 && (nextLvlDown == -1 || nextSep < nextLvlDown)) {
+				return nextSep;
+			}
+			
+			if(nextLvlDown != -1 && ( nextLvlDown < nextLvlUp || nextLvlUp == -1) ) {
+				index = nextLvlDown+1;
+				level++;
+			} else if(nextLvlUp != -1) {
+				index = nextLvlUp+1;
+				if(level > 0) level--;
+			} else {
+				return -1;
+			}
+		}
+		return -1;
+	}
+	
+	/**
+	 * see {@link #findStringPositionOnLevelZero(searchField, TO_STRING_SEPARATOR, TO_STRING_LEVEL_DOWN, TO_STRING_LEVEL_UP, beginIndex)}
+	 */
+	private static int findSeparatorOnLvlZero(String searchField, int beginIndex) {
+		return findStringPositionOnLevelZero(searchField, TO_STRING_SEPARATOR, TO_STRING_LEVEL_DOWN, TO_STRING_LEVEL_UP, beginIndex);
+	}
+	
+	private static String buildAlphabetRepresentationByArguments(
+			String alphaID, String... argList) {
+		StringBuilder result = new StringBuilder();
+		
+		result.append(alphaID + ALPHABET_TYPE_SEPARATOR);
+		/*
+		 * argument format examples (assuming that the separator is "||"):
+		 *  - arg1						assuming arg1 has no "||" in it
+		 *  - arg1||arg2				assuming each arg has no "||" in it
+		 *  - ((arg1))||arg2			assuming only arg1 has a "||" in it
+		 * ...
+		 */
+		
+		String argWithoutSeparatorsMask = "%s";
+		String argWithSeparatorsMask = TO_STRING_LEVEL_DOWN + "%s" + TO_STRING_LEVEL_UP;
+		
+		for(int i=0; i<argList.length; i++) {
+			if(i > 0) result.append(StringAlphabetFactory.TO_STRING_SEPARATOR);
+			if(findSeparatorOnLvlZero(argList[i], 0) != -1) {
+				result.append(String.format(argWithSeparatorsMask, argList[i]));
+			} else {
+				result.append(String.format(argWithoutSeparatorsMask, argList[i]));
+			}
+		}
+		
+		return result.toString();
+	}
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+		AtomAlphabet a_z = new AtomAlphabet("abcdefghijklmnopqrstuvwxyz");
+		AtomAlphabet A_Z = new AtomAlphabet("abcdefghijklmnopqrstuvwxyz".toUpperCase());
+		AtomAlphabet digits = new AtomAlphabet("0123456789");
+		AtomAlphabet random = new AtomAlphabet("asjhgjkghjghgkjKJJHKJ§/(/");
+		
+		System.out.println(a_z);
+		System.out.println(A_Z);
+		System.out.println(digits);
+		System.out.println(random);
+		System.out.println(StringAlphabetFactory.stringToList(random.getCharacterSet()));
+
+		CompositeAlphabet printable = new CompositeAlphabet(a_z, A_Z, digits, random);
+		
+		System.out.println(buildStringRepresentation(printable));
+		
+		for(Character c: a_z.getCharacterSet()) {
+			if(! printable.contains(c)) System.out.println("!!");
+		}
+		for(Character c: A_Z.getCharacterSet()) {
+			if(! printable.contains(c)) System.out.println("!!");
+		}
+		for(Character c: digits.getCharacterSet()) {
+			if(! printable.contains(c)) System.out.println("!!");
+		}
+		
+	}
+
+	protected static List<Character> stringToList(char[] characters) {
+		List<Character> list = new LinkedList<Character>();
+		for(char c: characters) list.add(c);
+		return list;
+	}
+
+	protected static List<Character> stringToList(String characters) {
+		return stringToList(characters.toCharArray());
+	}
+
+	protected static String listToString(List<Character> input) {
+		StringBuffer result = new StringBuffer();
+		for(Character c: input) result.append(c);
+		return result.toString();
+	}
+
+	protected static char[] toCharArray(List<Character> input) {
+		char[] result = new char[input.size()];
+		for(int i=0; i<input.size(); i++) result[i] = input.get(i);
+		return result;
+	}	
+	
+	
+}
