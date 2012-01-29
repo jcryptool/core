255b33e358611bfb17c56c99225f25221d1acb16
Dominik Schadow <dominikschadow@googlemail.com>
2012-01-29 16:42:41 +0100

    Code style
    Signed-off-by: Dominik Schadow <dominikschadow@googlemail.com>

--- org.jcryptool.actions.ui/src/org/jcryptool/actions/ui/handler/StartHandler.java
+++ org.jcryptool.actions.ui/src/org/jcryptool/actions/ui/handler/StartHandler.java
@@ -2,9 +2,8 @@
 /*******************************************************************************
  * Copyright (c) 2010 JCrypTool team and contributors
  *
- * All rights reserved. This program and the accompanying materials are made
- * available under the terms of the Eclipse Public License v1.0 which
- * accompanies this distribution, and is available at
+ * All rights reserved. This program and the accompanying materials are made available under the terms of the Eclipse
+ * Public License v1.0 which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *******************************************************************************/
 // -----END DISCLAIMER-----
@@ -58,183 +57,178 @@ import org.jcryptool.crypto.keystore.backend.KeyStoreAlias;
  */
 public class StartHandler extends AbstractHandler {
 
-	final Display display = Display.getCurrent();
+    final Display display = Display.getCurrent();
 
-	public Object execute(final ExecutionEvent event) throws ExecutionException {
-		final ActionView view = (ActionView) HandlerUtil.getActivePart(event);
-		if (!view.hasContent()) {
-			MessageDialog.openInformation(HandlerUtil.getActiveShell(event),
-					Messages.StartHandler_0, Messages.StartHandler_1);
-		}
-		else {
-			final ActionItem a = (ActionItem)((IStructuredSelection)(view.getViewer().getSelection())).getFirstElement();
-			Job job = new Job(Messages.StartHandler_5) {
-				public IStatus run(final IProgressMonitor monitor) {
-					return execute(view, a, HandlerUtil.getActiveShell(event), monitor);
-				}
-			};
-			job.setUser(true);
-			job.schedule();
-		}
+    public Object execute(final ExecutionEvent event) throws ExecutionException {
+        final ActionView view = (ActionView) HandlerUtil.getActivePart(event);
+        if (!view.hasContent()) {
+            MessageDialog.openInformation(HandlerUtil.getActiveShell(event), Messages.StartHandler_0,
+                    Messages.StartHandler_1);
+        } else {
+            final ActionItem a = (ActionItem) ((IStructuredSelection) (view.getViewer().getSelection()))
+                    .getFirstElement();
+            Job job = new Job(Messages.StartHandler_5) {
+                public IStatus run(final IProgressMonitor monitor) {
+                    return execute(view, a, HandlerUtil.getActiveShell(event), monitor);
+                }
+            };
+            job.setUser(true);
+            job.schedule();
+        }
 
-		return null;
-	}
+        return null;
+    }
 
+    public IStatus execute(final ActionView view, final ActionItem startItem, final Shell shell,
+            IProgressMonitor monitor) {
+        ActionCascade ac = view.getActionCascade();
+        final TableViewer viewer = view.getViewer();
 
-	public IStatus execute (final ActionView view, final ActionItem startItem, final Shell shell, IProgressMonitor monitor) {
-		ActionCascade ac = view.getActionCascade();
-		final TableViewer viewer = view.getViewer();
+        LogUtil.logInfo("Running Action Cascade " + ac.getName()); //$NON-NLS-1$
 
-		LogUtil.logInfo("Running Action Cascade " + ac.getName()); //$NON-NLS-1$
-
-		boolean start = false;
-		for (final ActionItem a : ac.getAllItems()) {
+        boolean start = false;
+        for (final ActionItem a : ac.getAllItems()) {
             int steps = ac.getSize();
 
-			//Start the execution at the first selected element
-			if (a == startItem ) {
-				start = true;
-				monitor.beginTask(a.getActionName(), steps);
-			}
-			else steps --;
+            // Start the execution at the first selected element
+            if (a == startItem) {
+                start = true;
+                monitor.beginTask(a.getActionName(), steps);
+            } else
+                steps--;
 
-			if (start){
-				if (monitor.isCanceled()) {
+            if (start) {
+                if (monitor.isCanceled()) {
                     return Status.CANCEL_STATUS;
                 }
 
-				try {
-					boolean executed = false;
+                try {
+                    boolean executed = false;
 
-					// Set focus on moved row. Just look and feel ...
-					display.asyncExec(new Runnable() {
-						public void run() {
-							viewer.setSelection(new StructuredSelection(a), true);
-						}
-					});
+                    // Set focus on moved row. Just look and feel ...
+                    display.asyncExec(new Runnable() {
+                        public void run() {
+                            viewer.setSelection(new StructuredSelection(a), true);
+                        }
+                    });
 
-					OperationsPlugin op = OperationsPlugin.getDefault();
-					IAction[] actions = op.getAlgorithmsManager().getShadowAlgorithmActions();
+                    OperationsPlugin op = OperationsPlugin.getDefault();
+                    IAction[] actions = op.getAlgorithmsManager().getShadowAlgorithmActions();
 
-					// Try to find an CryptoAlgorithm-Plug-in (Classic or Modern)
-					for (int i = 0, length = actions.length; i < length; i++) {
-						if (a.getPluginId().equals(actions[i].getId())) {
-							((ShadowAlgorithmAction) actions[i]).run(convert(a));
-							executed = true;
-						}
-					}
+                    // Try to find an CryptoAlgorithm-Plug-in (Classic or Modern)
+                    for (int i = 0, length = actions.length; i < length; i++) {
+                        if (a.getPluginId().equals(actions[i].getId())) {
+                            ((ShadowAlgorithmAction) actions[i]).run(convert(a));
+                            executed = true;
+                        }
+                    }
 
-					// Try to find a FlexiProvider algorithm
-					if (!executed) {
-						LogUtil.logInfo("Trying to execute FlexiProvider algorithm"); //$NON-NLS-1$
-						for (RegistryType rt : RegistryType.values()) {
-							if (rt.getName().equals(a.getPluginId())) {
-								if(!KeyStorePlugin.isInitialized()) KeyStorePlugin.initialize();
+                    // Try to find a FlexiProvider algorithm
+                    if (!executed) {
+                        LogUtil.logInfo("Trying to execute FlexiProvider algorithm"); //$NON-NLS-1$
+                        for (RegistryType rt : RegistryType.values()) {
+                            if (rt.getName().equals(a.getPluginId())) {
+                                if (!KeyStorePlugin.isInitialized())
+                                    KeyStorePlugin.initialize();
 
-								AlgorithmDescriptor descriptor = new AlgorithmDescriptor(a.getActionName(), rt, null);
+                                AlgorithmDescriptor descriptor = new AlgorithmDescriptor(a.getActionName(), rt, null);
 
-								if (a.getParam("algorithm type").equals(RegistryType.BLOCK_CIPHER.getName())) { //$NON-NLS-1$
-									descriptor = new BlockCipherDescriptor(descriptor.getAlgorithmName(),
-											a.getParam("mode"), a.getParam("padding scheme"), null, null); //$NON-NLS-1$ //$NON-NLS-2$
-								}
-								if (a.getParam("algorithm type").equals(RegistryType.SECURE_RANDOM.getName())) { //$NON-NLS-1$
-									//TODO Get property "alphabet" from item
-									byte[][] alphabet = null;
-									if (alphabet == null){
-										descriptor = new SecureRandomDescriptor(descriptor.getAlgorithmName(),
-												Integer.parseInt(a.getParam("random size"))); //$NON-NLS-1$
-									}
+                                if (a.getParam("algorithm type").equals(RegistryType.BLOCK_CIPHER.getName())) { //$NON-NLS-1$
+                                    descriptor = new BlockCipherDescriptor(descriptor.getAlgorithmName(),
+                                            a.getParam("mode"), a.getParam("padding scheme"), null, null); //$NON-NLS-1$ //$NON-NLS-2$
+                                } else if (a.getParam("algorithm type").equals(RegistryType.SECURE_RANDOM.getName())) { //$NON-NLS-1$
+                                    // TODO Get property "alphabet" from item
+                                    byte[][] alphabet = null;
+                                    if (alphabet == null) {
+                                        descriptor = new SecureRandomDescriptor(descriptor.getAlgorithmName(),
+                                                Integer.parseInt(a.getParam("random size"))); //$NON-NLS-1$
+                                    }
+                                }
 
-								}
+                                final IFlexiProviderOperation operation = new IntegratorOperation(descriptor);
+                                operation.setInput("<Editor>"); //$NON-NLS-1$
+                                operation.setOutput("<Editor>"); //$NON-NLS-1$
+                                operation.setSignature(a.getParam("signature")); //$NON-NLS-1$
 
+                                if ("encrypt".equals(a.getActionType())) { //$NON-NLS-1$
+                                    operation.setOperation(OperationType.ENCRYPT);
+                                } else if ("decrypt".equals(a.getActionType())) { //$NON-NLS-1$
+                                    operation.setOperation(OperationType.DECRYPT);
+                                } else if ("sign".equals(a.getActionType())) { //$NON-NLS-1$
+                                    operation.setOperation(OperationType.SIGN);
+                                } else if ("verify".equals(a.getActionType())) { //$NON-NLS-1$
+                                    operation.setOperation(OperationType.VERIFY);
+                                } else {
+                                    operation.setOperation(OperationType.UNKNOWN);
+                                }
 
-								final IFlexiProviderOperation operation = new IntegratorOperation(descriptor);
-								operation.setInput("<Editor>"); //$NON-NLS-1$
-								operation.setOutput("<Editor>"); //$NON-NLS-1$
-								operation.setSignature(a.getParam("signature")); //$NON-NLS-1$
+                                String alias = a.getParam("key alias"); //$NON-NLS-1$
+                                if (alias != null) {
+                                    operation.setKeyStoreAlias(new KeyStoreAlias(alias));
+                                }
 
-								if ("encrypt".equals(a.getActionType())) { //$NON-NLS-1$
-									operation.setOperation(OperationType.ENCRYPT);
-								} else if ("decrypt".equals(a.getActionType())) { //$NON-NLS-1$
-									operation.setOperation(OperationType.DECRYPT);
-								} else if ("sign".equals(a.getActionType())) { //$NON-NLS-1$
-									operation.setOperation(OperationType.SIGN);
-								} else if ("verify".equals(a.getActionType())) { //$NON-NLS-1$
-									operation.setOperation(OperationType.VERIFY);
-								} else {
-									operation.setOperation(OperationType.UNKNOWN);
-								}
+                                String password = a.getParam("key password"); //$NON-NLS-1$
+                                if (password != null) {
+                                    operation.setPassword(password.toCharArray());
+                                }
 
-								String alias = a.getParam("key alias"); //$NON-NLS-1$
-								if (alias != null) {
-									operation.setKeyStoreAlias(new KeyStoreAlias(alias));
-								}
+                                display.asyncExec(new Runnable() {
+                                    public void run() {
+                                        PerformOperationManager.getInstance().firePerformOperation(operation);
+                                    }
+                                });
+                                executed = true;
+                            }
+                        }
+                    }
+                    if (!executed) {
+                        throw new Exception(Messages.StartHandler_2 + a.getPluginId());
 
-								String password = a.getParam("key password");  //$NON-NLS-1$
-								if (password != null) {
-									operation.setPassword(password.toCharArray());
-								}
+                    }
+                } catch (final Exception e) {
+                    if (ActionsUIPlugin.getDefault().getPreferenceStore()
+                            .getBoolean(PreferenceConstants.P_IGNORE_ERRORS)) {
+                        LogUtil.logError(ActionsUIPlugin.PLUGIN_ID, a.getActionName() + ": " + e.getMessage(), e, false); //$NON-NLS-1$
+                        if (!ActionsUIPlugin.getDefault().getPreferenceStore()
+                                .getBoolean(PreferenceConstants.P_DONT_SHOW_MESSAGES)) {
+                            display.syncExec(new Runnable() {
+                                public void run() {
+                                    MessageDialog.openWarning(shell, Messages.StartHandler_3, e.getMessage());
+                                }
+                            });
+                        }
+                    } else {
+                        display.asyncExec(new Runnable() {
+                            public void run() {
+                                MessageDialog.openError(shell, Messages.StartHandler_3, e.getMessage()
+                                        + Messages.StartHandler_4); //$NON-NLS-1$
+                            }
+                        });
+                        return Status.CANCEL_STATUS;
+                    }
+                } finally {
+                    monitor.worked(ac.getSize() - steps);
+                }
+            }
+        }
+        monitor.done();
+        return Status.OK_STATUS;
+    }
 
-								display.asyncExec(new Runnable() {
-									public void run() {
-								PerformOperationManager.getInstance().firePerformOperation(operation);
-									}
-								});
-								executed = true;
-							}
-						}
-					}
-					if (!executed){
-						throw new Exception(Messages.StartHandler_2+a.getPluginId());
+    public IDataObject convert(ActionItem actionItem) {
+        IDataObject dataobject = DataObjectConverter.hashtableToProperties(actionItem.getParams(),
+                actionItem.getDataObjectType());
 
-					}
-				}
-				catch (final Exception e){
-					if (ActionsUIPlugin.getDefault().getPreferenceStore()
-							.getBoolean(PreferenceConstants.P_IGNORE_ERRORS)){
-						LogUtil.logError(ActionsUIPlugin.PLUGIN_ID , a.getActionName()+": "+e.getMessage(), e, false); //$NON-NLS-1$
-						if (!ActionsUIPlugin.getDefault().getPreferenceStore()
-								.getBoolean(PreferenceConstants.P_DONT_SHOW_MESSAGES)){
-							display.syncExec(new Runnable() {
-								public void run() {
-							       MessageDialog.openWarning(shell, Messages.StartHandler_3, e.getMessage());
-								}
-							});
-						}
-					}
-					else {
-						display.asyncExec(new Runnable() {
-							public void run() {
-					        	MessageDialog.openError(shell, Messages.StartHandler_3, e.getMessage()+Messages.StartHandler_4); //$NON-NLS-1$
-							}
-						});
-						return Status.CANCEL_STATUS;
-					}
-				}
-				finally {
-					monitor.worked(ac.getSize()-steps);
-				}
-			}
-		}
-		monitor.done();
-		return Status.OK_STATUS;
-	}
+        if (dataobject instanceof ClassicDataObject) {
+            AbstractAlphabet alphabet = AlphabetsManager.getInstance().getAlphabetByName(actionItem.getAlphabet());
+            ((ClassicDataObject) dataobject).setAlphabet(alphabet);
+            if ("encrypt".equals(actionItem.getActionType())) { //$NON-NLS-1$
+                ((ClassicDataObject) dataobject).setOpmode(AbstractAlgorithm.ENCRYPT_MODE);
+            } else if ("decrypt".equals(actionItem.getActionType())) { //$NON-NLS-1$
+                ((ClassicDataObject) dataobject).setOpmode(AbstractAlgorithm.DECRYPT_MODE);
+            }
+        }
 
-	public IDataObject convert(ActionItem actionItem) {
-		IDataObject dataobject = DataObjectConverter.hashtableToProperties(actionItem.getParams(),
-				actionItem.getDataObjectType());
-
-		if (dataobject instanceof ClassicDataObject) {
-			AbstractAlphabet alphabet = AlphabetsManager.getInstance().getAlphabetByName(
-					actionItem.getAlphabet());
-			((ClassicDataObject) dataobject).setAlphabet(alphabet);
-			if ("encrypt".equals(actionItem.getActionType())) { //$NON-NLS-1$
-				((ClassicDataObject) dataobject).setOpmode(AbstractAlgorithm.ENCRYPT_MODE);
-			} else if ("decrypt".equals(actionItem.getActionType())) { //$NON-NLS-1$
-				((ClassicDataObject) dataobject).setOpmode(AbstractAlgorithm.DECRYPT_MODE);
-			}
-		}
-
-		return dataobject;
-	}
+        return dataobject;
+    }
 }
