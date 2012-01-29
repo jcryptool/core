5075d085ed1585bb1ed84f81ea3d8a287ac25486
Dominik Schadow <dominikschadow@googlemail.com>
2012-01-29 16:57:09 +0100

    Removed public static void main method
    Signed-off-by: Dominik Schadow <dominikschadow@googlemail.com>

--- org.jcryptool.core.cryptosystem/src/org/jcryptool/core/cryptosystem/vigenere/TextVigenere.java
+++ org.jcryptool.core.cryptosystem/src/org/jcryptool/core/cryptosystem/vigenere/TextVigenere.java
@@ -1,42 +1,18 @@
 package org.jcryptool.core.cryptosystem.vigenere;
 
 import org.jcryptool.core.cryptosystem.core.Cryptosystem;
-import org.jcryptool.core.cryptosystem.core.exception.ElementNotInAlphabetException;
 import org.jcryptool.core.cryptosystem.core.text.CharAlphabet;
 import org.jcryptool.core.cryptosystem.core.text.TextCompatibleCryptosystemWrapper;
 import org.jcryptool.core.cryptosystem.core.text.TextConverter;
 
-
 public class TextVigenere extends TextCompatibleCryptosystemWrapper<Character, Character, VigenereKey<Character>> {
 
-	public TextVigenere(CharAlphabet charAlphabet) {
-		super(createVigenereSystem(charAlphabet), TextConverter.CHAR_TEXT_CONVERTER, TextConverter.CHAR_TEXT_CONVERTER);
-	}
+    public TextVigenere(CharAlphabet charAlphabet) {
+        super(createVigenereSystem(charAlphabet), TextConverter.CHAR_TEXT_CONVERTER, TextConverter.CHAR_TEXT_CONVERTER);
+    }
 
-	private static Cryptosystem<Character, Character, VigenereKey<Character>> createVigenereSystem(
-			CharAlphabet charAlphabet) {
-		return new VigenereCryptosystem<Character>(charAlphabet);
-	}
-	
-	public static void main(String[] args) throws ElementNotInAlphabetException {
-		CharAlphabet alphabet = 
-			new CharAlphabet("abcdefghijklmnopqrstuvwxyz ");
-		TextVigenere vigenereCryptosystem = 
-			new TextVigenere(alphabet);
-		VigenereKey<Character> key = 
-			new VigenereKey<Character>(alphabet.parseString("key"), alphabet);
-		
-		String plaintext = "this is a test text";
-		String encryptedText;
-		String decryptedText;
-		
-		encryptedText = vigenereCryptosystem.encryptTextToText(plaintext, key);
-		decryptedText = vigenereCryptosystem.decryptTextToText(encryptedText, key);
-		
-		System.out.println("Plaintext:                   " + plaintext);
-		System.out.println("encrypt(plaintext,  'key') = " + encryptedText);
-		System.out.println("decrypt(ciphertext, 'key') = " + decryptedText);
-	}
-
-
+    private static Cryptosystem<Character, Character, VigenereKey<Character>> createVigenereSystem(
+            CharAlphabet charAlphabet) {
+        return new VigenereCryptosystem<Character>(charAlphabet);
+    }
 }
