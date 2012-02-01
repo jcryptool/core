95c4318509814143e80d718dd382512a436b5bd9
Johannes Späth <jojospaeth@gmx.de>
2012-02-01 14:16:23 +0100

    Bug in implementation of "play selected sequence" in the optimal strategy wizard corrected.

--- org.jcryptool.games.numbershark/src/org/jcryptool/games/numbershark/handler/OptimalStrategyHandler.java
+++ org.jcryptool.games.numbershark/src/org/jcryptool/games/numbershark/handler/OptimalStrategyHandler.java
@@ -24,63 +24,61 @@ import org.jcryptool.games.numbershark.views.NumberSharkView;
 
 /**
  * This handler starts the wizard for calculating optimal strategies.
- *
+ * 
  * @author Johannes Späth
  * @version 0.9.5
  */
 public class OptimalStrategyHandler extends AbstractHandler {
 	public static final int PLAY = 9998;
-    public Object execute(ExecutionEvent event) throws ExecutionException {
-        if (HandlerUtil.getActivePart(event) instanceof NumberSharkView) {
 
-            NumberSharkView view = ((NumberSharkView) HandlerUtil.getActivePart(event));
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		if (HandlerUtil.getActivePart(event) instanceof NumberSharkView) {
 
-            OptimalStrategyDialog optStrat = new OptimalStrategyDialog(HandlerUtil.getActiveShell(event));
-            optStrat.create();
-            optStrat.open();
+			NumberSharkView view = ((NumberSharkView) HandlerUtil
+					.getActivePart(event));
 
-            if (optStrat.getReturnCode() == IDialogConstants.OK_ID) {
+			OptimalStrategyDialog optStrat = new OptimalStrategyDialog(
+					HandlerUtil.getActiveShell(event));
+			optStrat.create();
+			optStrat.open();
 
-            	ResultDialog calculate;
-            	if(optStrat.getCalculateStrategy()){
-            		calculate = new ResultDialog(HandlerUtil.getActiveShell(event), true);
-            		calculate.setBounds(optStrat.getMin() , optStrat.getMax());
-            	} else {
-            		calculate = new ResultDialog(HandlerUtil.getActiveShell(event), false);
-            	}
-            	calculate.open();
-            	
-            	if(calculate.getReturnCode() == PLAY){
-            		ArrayList<Integer> playSeq = calculate.getPlaySequence();
-                    view.cleanPlayingField();
-                    view.createPlayingField(playSeq.get(0));
-                    for (int i = 1; i < playSeq.size(); i++){
-                    	view.deactivateNumber(playSeq.get(i));
-                    }
-                    ArrayList<Integer> sharkMealList = view.getSharkMealList();
-        			int[] lostNumbers = new int[sharkMealList.size()];
+			if (optStrat.getReturnCode() == IDialogConstants.OK_ID) {
+				
+				ResultDialog calculate;
+				if (optStrat.getCalculateStrategy()) {
+					calculate = new ResultDialog(
+							HandlerUtil.getActiveShell(event), true);
+					calculate.setBounds(optStrat.getMin(), optStrat.getMax());
+				} else {
+					calculate = new ResultDialog(
+							HandlerUtil.getActiveShell(event), false);
+				}
+				calculate.open();
 
-    				int tabFolderIndex = view.getSelectedTabFolderIndex();
-                    for (int i = 0; i < sharkMealList.size(); i++){
-                    	lostNumbers[i] = sharkMealList.get(i);
+				if (calculate.getReturnCode() == PLAY) {
+					ArrayList<Integer> playSeq = calculate.getPlaySequence();
+					view.cleanPlayingField();
+					view.createPlayingField(playSeq.get(0));
+					for (int i = 1; i < playSeq.size(); i++) {
+						view.deactivateNumber(playSeq.get(i));
+					}
+					ArrayList<Integer> sharkMealList = view.getSharkMealList();
+					int[] lostNumbers = new int[sharkMealList.size()];
 
-        				if (tabFolderIndex * 40 < lostNumbers[i]
-        						&& i < (tabFolderIndex + 1) * 40 + 1) {
-        					view.disableNumber(lostNumbers[i] - 1);
-        				}
-                    }
+					for (int i = 0; i < sharkMealList.size(); i++) {
+						lostNumbers[i] = sharkMealList.get(i);
+						view.disableNumber(lostNumbers[i] - 1);
+					}
 
-                	view.addMoveToTable(0, lostNumbers);
-            		CommandStateChanger commandStateChanger = new CommandStateChanger();
-            		commandStateChanger.chageCommandState(
-            				CommandState.Variable.SHARKMEAL_STATE,
-            				CommandState.State.SHARKMEAL_DISABLED);
-            	}
-            	
-            }
-        }
+					view.addMoveToTable(0, lostNumbers);
+					CommandStateChanger commandStateChanger = new CommandStateChanger();
+					commandStateChanger.chageCommandState(
+							CommandState.Variable.SHARKMEAL_STATE,
+							CommandState.State.SHARKMEAL_DISABLED);
+				}
 
-             
-        return null;
-    }
+			}
+		}
+		return null;
+	}
 }
