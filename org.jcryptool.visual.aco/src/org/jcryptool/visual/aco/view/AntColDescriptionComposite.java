9c5629b3ee103697e098b56bdc38c9517780a70b
johannes.spaeth <jojospaeth@gmx.de>
2012-10-05 23:05:48 +0200

    Ant Colony Optimization update

---
+++ org.jcryptool.visual.aco/src/org/jcryptool/visual/aco/view/AntColDescriptionComposite.java
@@ -0,0 +1,108 @@
+// -----BEGIN DISCLAIMER-----
+/*******************************************************************************
+ * Copyright (c) 2011 JCrypTool team and contributors
+ *
+ * All rights reserved. This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+// -----END DISCLAIMER-----
+package org.jcryptool.visual.aco.view;
+
+import java.util.Observable;
+import java.util.Observer;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.ScrolledComposite;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Label;
+import org.jcryptool.visual.aco.model.Model;
+
+/**
+ * Feld fuer die Informationstexte im Tutorial zur Kryptanalyse von
+ * Transpositionschiffren mit einem Ameisenalgorithmus.
+ * 
+ * @author Philipp Blohm
+ * @version 03.08.07
+ * 
+ */
+public class AntColDescriptionComposite extends Composite {
+
+	private Label text;
+	private Model m;
+	private boolean isText3and4 = false;
+	private Color eyeColor = new Color(this.getDisplay(), 255, 255, 255);
+
+	/**
+	 * Konstruktor. Erhaelt Model, setzt grundlegende Einstellungen.
+	 * 
+	 * @param m
+	 *            Model mit den Daten des Tutorials
+	 * @param c
+	 *            Parent
+	 */
+	public AntColDescriptionComposite(Model m, Composite c) {
+		super(c, SWT.NONE);
+		this.m = m;
+
+		this.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+		this.setLayout(new FillLayout());
+
+		Group descriptionGroup = new Group(this, SWT.NONE);
+		descriptionGroup.setLayout(new FillLayout());
+		descriptionGroup.setText(Messages.Description_title);
+		descriptionGroup.setToolTipText(Messages.Description_tooltip);
+
+		ScrolledComposite sc = new ScrolledComposite(descriptionGroup, SWT.H_SCROLL
+				| SWT.V_SCROLL);
+		Composite innerComp = new Composite(sc, SWT.NONE);
+		innerComp.setLayout(new FillLayout());
+		sc.setContent(innerComp);
+		sc.setMinSize(innerComp.computeSize(SWT.DEFAULT, SWT.DEFAULT));
+		text = new Label(innerComp, SWT.WRAP);
+		updateText();
+		sc.setExpandHorizontal(true);
+		sc.setExpandVertical(true);
+		layout();
+	}
+
+	/**
+	 * Reagiert auf Aenderungen des Models, liest den jeweils benoetigten Text
+	 * aus einer Datei und schreibt diesen in sein Textfeld.
+	 */
+	public void updateText() {
+		String s = ""; //$NON-NLS-1$
+		int nr = m.getState();
+		switch (nr) {
+		case 0:
+			s = Messages.Info_description1;
+			isText3and4 = false;
+			break; //$NON-NLS-1$
+		case 1:
+			s = Messages.Info_description2;
+			isText3and4 = false;
+			break; //$NON-NLS-1$
+		case 2:
+			s = Messages.Info_description3;
+			if (m.isFinishCycle()) {
+				s += "\n"+ Messages.Info_description4;
+				isText3and4 = true;
+			}
+			break; //$NON-NLS-1$
+		case 3:
+			if (isText3and4) {
+				return;
+			}
+			s = Messages.Info_description4;
+			break; //$NON-NLS-1$
+		}
+		text.setText(s);
+		layout();
+	}
+
+}
---
+++ org.jcryptool.visual.aco/src/org/jcryptool/visual/aco/view/Messages.java
@@ -0,0 +1,80 @@
+// -----BEGIN DISCLAIMER-----
+/*******************************************************************************
+ * Copyright (c) 2011 JCrypTool team and contributors
+ *
+ * All rights reserved. This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+// -----END DISCLAIMER-----
+package org.jcryptool.visual.aco.view;
+
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+
+import org.eclipse.osgi.util.NLS;
+
+public class Messages {
+	private static final String BUNDLE_NAME = "org.jcryptool.visual.aco.view.messages"; //$NON-NLS-1$
+
+	public static String Func_allSteps;
+	public static String Func_alpha;
+	public static String Func_analyseCreated;
+	public static String Func_analyseGiven;
+	public static String Func_analysis;
+	public static String Func_animation;
+	public static String Func_antAnalysis;
+	public static String Func_beta;
+	public static String Func_cycle;
+	public static String Func_doWithNewAnt;
+	public static String Func_encryption;
+	public static String Func_error;
+	public static String Func_evap;
+	public static String Func_initial_plaintext;
+	public static String Func_keyLength;
+	public static String Func_newAnt;
+	public static String Func_permutation;
+	public static String Func_plaintext;
+	public static String Func_ciphertext;
+	public static String Func_general;
+	public static String Func_keytext;
+	public static String Func_oneStep;
+	public static String Func_proceed;
+	public static String Func_proceedToAnalysis;
+	public static String Func_reset;
+	public static String Func_analyseConfiguration;
+	public static String Func_analyseGroupLabel;
+	public static String Func_step;
+	public static String Info_description1;
+	public static String Info_description2;
+	public static String Info_description3;
+	public static String Info_description4;
+	public static String Func_possibleInputs;
+	public static String Func_textLanguage;
+	public static String Func_analysisType;
+	public static String Func_settings;
+	public static String Show_decryptedBestKnown;
+	public static String Show_decryptedByAnt1;
+	public static String Show_decryptedByAnt2;
+	public static String Show_encryptedText;
+	public static String Show_encryptionKey;
+	public static String Show_permutationMatrix;
+	public static String Show_pheromoneMatrix;
+	public static String Show_wrongInputTextSize;
+	public static String Description_title;
+	public static String Description_tooltip;
+	public static String Func_radioCompleteRound;
+	public static String Control_language1;
+	public static String Control_language2;
+	public static String Control_language1_short;
+	public static String Control_language2_short;
+	public static String Analysis_multipleCycles;
+
+	private Messages() {
+	}
+
+	static {
+		// initialize resource bundle
+		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
+	}
+}
