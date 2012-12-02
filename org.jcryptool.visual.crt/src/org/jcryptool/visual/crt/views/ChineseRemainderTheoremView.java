b8baaa17ff01b53d957e624e80bc84c0ee6f139c
Dominik Schadow <dominikschadow@googlemail.com>
2012-12-02 12:31:44 +0100

    Issue 55: F1 does not link to the existing help pages
    Added context help to view
    
    Signed-off-by: Dominik Schadow <dominikschadow@googlemail.com>

--- org.jcryptool.visual.crt/src/org/jcryptool/visual/crt/views/ChineseRemainderTheoremView.java
+++ org.jcryptool.visual.crt/src/org/jcryptool/visual/crt/views/ChineseRemainderTheoremView.java
@@ -1,9 +1,9 @@
 // -----BEGIN DISCLAIMER-----
 /*******************************************************************************
  * Copyright (c) 2011 JCrypTool Team and Contributors
- *
- * All rights reserved. This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License v1.0 which accompanies this distribution, and is available at
+ * 
+ * All rights reserved. This program and the accompanying materials are made available under the terms of the Eclipse
+ * Public License v1.0 which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *******************************************************************************/
 // -----END DISCLAIMER-----
@@ -21,6 +21,7 @@ import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.part.ViewPart;
 import org.jcryptool.core.util.constants.IConstants;
 import org.jcryptool.core.util.directories.DirectoryService;
+import org.jcryptool.visual.crt.ChineseRemainderTheoremPlugin;
 import org.jcryptool.visual.crt.export.FileExporter;
 
 /**
@@ -29,137 +30,132 @@ import org.jcryptool.visual.crt.export.FileExporter;
  */
 public class ChineseRemainderTheoremView extends ViewPart implements Constants {
 
-	private Action exportToLatexAction;
-	private Action exportToCSVAction;
-	private Action exportToPdfAction;
+    private Action exportToLatexAction;
+    private Action exportToCSVAction;
+    private Action exportToPdfAction;
 
-	private StackLayout layout;
-	private CRTGroup CRTGroup;
-	private Composite parent;
-	private boolean reset = false;
+    private StackLayout layout;
+    private CRTGroup CRTGroup;
+    private Composite parent;
+    private boolean reset = false;
 
-	/**
-	 * Create contents of the view part
-	 * 
-	 * @param parent
-	 */
-	@Override
-	public void createPartControl(Composite parent) {
-		this.parent = parent;
-		PlatformUI.getWorkbench().getHelpSystem().setHelp(parent, "1");
+    /**
+     * Create contents of the view part
+     * 
+     * @param parent
+     */
+    @Override
+    public void createPartControl(Composite parent) {
+        this.parent = parent;
 
-		layout = new StackLayout();
-		parent.setLayout(layout);
+        layout = new StackLayout();
+        parent.setLayout(layout);
 
-		CRTGroup = new CRTGroup(parent, SWT.NONE, this);
-		layout.topControl = CRTGroup;
+        CRTGroup = new CRTGroup(parent, SWT.NONE, this);
+        layout.topControl = CRTGroup;
 
-		if (!reset) {
-			createActions();
-			initializeMenu();
-		}
-	}
+        if (!reset) {
+            createActions();
+            initializeMenu();
+        }
 
-	/**
-	 * Create the actions
-	 */
-	private void createActions() {
+        PlatformUI.getWorkbench().getHelpSystem()
+                .setHelp(parent.getShell(), ChineseRemainderTheoremPlugin.PLUGIN_ID + ".view");
+    }
 
-		exportToPdfAction = new Action(MESSAGE_EXPORT_PDF) {
-			@Override
-			public void run() {
-				FileDialog dialog = new FileDialog(Display.getCurrent()
-						.getActiveShell(), SWT.SAVE);
-				dialog.setFilterExtensions(new String[] { IConstants.PDF_FILTER_EXTENSION });
-				dialog.setFilterNames(new String[] { IConstants.PDF_FILTER_NAME });
-				dialog.setFilterPath(DirectoryService.getUserHomeDir());
-				dialog.setOverwrite(true);
-				String filename = dialog.open();
+    /**
+     * Create the actions
+     */
+    private void createActions() {
 
-				if (filename != null) {
-					FileExporter pdfExport = new FileExporter(
-							CRTGroup.getCrt(), filename);
-					pdfExport.exportToPDF();
-				}
+        exportToPdfAction = new Action(MESSAGE_EXPORT_PDF) {
+            @Override
+            public void run() {
+                FileDialog dialog = new FileDialog(Display.getCurrent().getActiveShell(), SWT.SAVE);
+                dialog.setFilterExtensions(new String[] {IConstants.PDF_FILTER_EXTENSION});
+                dialog.setFilterNames(new String[] {IConstants.PDF_FILTER_NAME});
+                dialog.setFilterPath(DirectoryService.getUserHomeDir());
+                dialog.setOverwrite(true);
+                String filename = dialog.open();
 
-			}
-		};
-		exportToPdfAction.setEnabled(false);
+                if (filename != null) {
+                    FileExporter pdfExport = new FileExporter(CRTGroup.getCrt(), filename);
+                    pdfExport.exportToPDF();
+                }
 
-		exportToCSVAction = new Action(MESSAGE_EXPORT_CSV) {
-			@Override
-			public void run() {
-				FileDialog dialog = new FileDialog(Display.getCurrent()
-						.getActiveShell(), SWT.SAVE);
-				dialog.setFilterExtensions(new String[] { IConstants.CSV_FILTER_EXTENSION });
-				dialog.setFilterNames(new String[] { IConstants.CSV_FILTER_NAME });
-				dialog.setFilterPath(DirectoryService.getUserHomeDir());
-				dialog.setOverwrite(true);
-				String filename = dialog.open();
+            }
+        };
+        exportToPdfAction.setEnabled(false);
 
-				if (filename != null) {
-					FileExporter csvExport = new FileExporter(
-							CRTGroup.getCrt(), filename);
-					csvExport.exportToCSV();
-				}
-			}
-		};
-		exportToCSVAction.setEnabled(false);
+        exportToCSVAction = new Action(MESSAGE_EXPORT_CSV) {
+            @Override
+            public void run() {
+                FileDialog dialog = new FileDialog(Display.getCurrent().getActiveShell(), SWT.SAVE);
+                dialog.setFilterExtensions(new String[] {IConstants.CSV_FILTER_EXTENSION});
+                dialog.setFilterNames(new String[] {IConstants.CSV_FILTER_NAME});
+                dialog.setFilterPath(DirectoryService.getUserHomeDir());
+                dialog.setOverwrite(true);
+                String filename = dialog.open();
 
-		exportToLatexAction = new Action(MESSAGE_EXPORT_LATEX) {
-			@Override
-			public void run() {
-				FileDialog dialog = new FileDialog(Display.getCurrent()
-						.getActiveShell(), SWT.SAVE);
-				dialog.setFilterExtensions(new String[] { IConstants.TEX_FILTER_EXTENSION });
-				dialog.setFilterNames(new String[] { IConstants.TEX_FILTER_NAME });
-				dialog.setFilterPath(DirectoryService.getUserHomeDir());
-				dialog.setOverwrite(true);
-				String filename = dialog.open();
+                if (filename != null) {
+                    FileExporter csvExport = new FileExporter(CRTGroup.getCrt(), filename);
+                    csvExport.exportToCSV();
+                }
+            }
+        };
+        exportToCSVAction.setEnabled(false);
 
-				if (filename != null) {
-					FileExporter latexExport = new FileExporter(
-							CRTGroup.getCrt(), filename);
-					latexExport.exportToLatex();
-				}
+        exportToLatexAction = new Action(MESSAGE_EXPORT_LATEX) {
+            @Override
+            public void run() {
+                FileDialog dialog = new FileDialog(Display.getCurrent().getActiveShell(), SWT.SAVE);
+                dialog.setFilterExtensions(new String[] {IConstants.TEX_FILTER_EXTENSION});
+                dialog.setFilterNames(new String[] {IConstants.TEX_FILTER_NAME});
+                dialog.setFilterPath(DirectoryService.getUserHomeDir());
+                dialog.setOverwrite(true);
+                String filename = dialog.open();
 
-			}
-		};
-		exportToLatexAction.setEnabled(false);
-		// Create the actions
-	}
+                if (filename != null) {
+                    FileExporter latexExport = new FileExporter(CRTGroup.getCrt(), filename);
+                    latexExport.exportToLatex();
+                }
 
-	/**
-	 * Initialize the menu
-	 */
-	private void initializeMenu() {
-		IMenuManager menuManager = getViewSite().getActionBars()
-				.getMenuManager();
+            }
+        };
+        exportToLatexAction.setEnabled(false);
+        // Create the actions
+    }
 
-		menuManager.add(exportToPdfAction);
-		menuManager.add(exportToCSVAction);
-		menuManager.add(exportToLatexAction);
-	}
+    /**
+     * Initialize the menu
+     */
+    private void initializeMenu() {
+        IMenuManager menuManager = getViewSite().getActionBars().getMenuManager();
 
-	public void enableMenu(boolean enable) {
-		exportToPdfAction.setEnabled(enable);
-		exportToLatexAction.setEnabled(enable);
-		exportToCSVAction.setEnabled(enable);
-	}
+        menuManager.add(exportToPdfAction);
+        menuManager.add(exportToCSVAction);
+        menuManager.add(exportToLatexAction);
+    }
 
-	public void reset() {
-		layout = null;
-		Control[] children = parent.getChildren();
-		for (Control control : children) {
-			control.dispose();
-		}
-		createPartControl(parent);
-		parent.layout();
-		reset = true;
-	}
+    public void enableMenu(boolean enable) {
+        exportToPdfAction.setEnabled(enable);
+        exportToLatexAction.setEnabled(enable);
+        exportToCSVAction.setEnabled(enable);
+    }
 
-	@Override
-	public void setFocus() {
-	}
+    public void reset() {
+        layout = null;
+        Control[] children = parent.getChildren();
+        for (Control control : children) {
+            control.dispose();
+        }
+        createPartControl(parent);
+        parent.layout();
+        reset = true;
+    }
+
+    @Override
+    public void setFocus() {
+    }
 
 }
