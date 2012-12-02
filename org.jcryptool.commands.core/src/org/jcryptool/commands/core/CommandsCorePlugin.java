98276223622bb3b8f0b98e8e58c8249e65555ebe
Dominik Schadow <dominikschadow@googlemail.com>
2012-12-02 12:35:28 +0100

    Removed duplicate disclaimer
    Signed-off-by: Dominik Schadow <dominikschadow@googlemail.com>

--- org.jcryptool.commands.core/src/org/jcryptool/commands/core/CommandsCorePlugin.java
+++ org.jcryptool.commands.core/src/org/jcryptool/commands/core/CommandsCorePlugin.java
@@ -9,15 +9,6 @@
 // -----END DISCLAIMER-----
 package org.jcryptool.commands.core;
 
-/*******************************************************************************
-* Copyright (c) 2010 JCrypTool Team and Contributors
-*
-* All rights reserved. This program and the accompanying materials
-* are made available under the terms of the Eclipse Public License v1.0
-* which accompanies this distribution, and is available at
-* http://www.eclipse.org/legal/epl-v10.html
-*******************************************************************************/
-//-----END DISCLAIMER-----
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
