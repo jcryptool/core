262a21a1f3c234e7087bbc944364948e46052b58
simlei <simlei@Simon>
2013-01-02 13:31:55 +0100

    added internationalizations for standard alphabet names

--- org.jcryptool.crypto.classic.alphabets/src/org/jcryptool/crypto/classic/alphabets/tools/Messages.java
+++ org.jcryptool.crypto.classic.alphabets/src/org/jcryptool/crypto/classic/alphabets/tools/Messages.java
@@ -4,6 +4,22 @@ import org.eclipse.osgi.util.NLS;
 
 public class Messages extends NLS {
 	private static final String BUNDLE_NAME = "org.jcryptool.crypto.classic.alphabets.tools.messages"; //$NON-NLS-1$
+	public static String AlphabetStore_alpha_adfgvx_long;
+	public static String AlphabetStore_alpha_adfgvx_short;
+	public static String AlphabetStore_alpha_ascii_long;
+	public static String AlphabetStore_alpha_ascii_short;
+	public static String AlphabetStore_alpha_lowlatin_long;
+	public static String AlphabetStore_alpha_lowlatin_short;
+	public static String AlphabetStore_alpha_playfair_long;
+	public static String AlphabetStore_alpha_playfair_short;
+	public static String AlphabetStore_alpha_uplatin_long;
+	public static String AlphabetStore_alpha_uplatin_short;
+	public static String AlphabetStore_alpha_uplolatin_long;
+	public static String AlphabetStore_alpha_uplolatin_short;
+	public static String AlphabetStore_alpha_xor32_long;
+	public static String AlphabetStore_alpha_xor32_short;
+	public static String AlphabetStore_alpha_xor64_long;
+	public static String AlphabetStore_alpha_xor64_short;
 	public static String AlphabetStore_extrachars;
 	static {
 		// initialize resource bundle
