762c5dda3f004afba1441729998d79f288efa261
Schnepf Christoph <ch.schnepf@gmail.com>
2013-01-10 20:14:22 +0100

    added JUnit-Tests for some classes

---
+++ org.jcryptool.visual.extendedrsa.tests/src/org/jcryptool/visual/extendedrsa/tests/SecureMessageTest.java
@@ -0,0 +1,45 @@
+//-----BEGIN DISCLAIMER-----
+/*******************************************************************************
+ * Copyright (c) 2013 JCrypTool Team and Contributors
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+//-----END DISCLAIMER-----
+
+package org.jcryptool.visual.extendedrsa.tests;
+
+import org.jcryptool.visual.extendedrsa.SecureMessage;
+import org.junit.Test;
+
+/**
+ * test for the class "SecureMessage.java"
+ * @author Christoph Schnepf, Patrick Zillner
+ *
+ */
+public class SecureMessageTest {
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testSecureMessage_null1() {
+		SecureMessage message = new SecureMessage(null, 0, null, null, null);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testSecureMessage_null2() {
+		SecureMessage message = new SecureMessage("asdf", 0, null, null, null);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testSecureMessage_null3() {
+		SecureMessage message = new SecureMessage("asdf", 1, null, null, null);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testSecureMessage_null4() {
+		SecureMessage message = new SecureMessage("asdf", 1, "sender", null, null);
+	}
+
+	//for the other methods, you need a valid recipient. But you only get a valid one, if JCrypTool is launched!
+}
