90f1891587c0f05fdc2565a8c2c029141ea67ff0
Florentina Grebe <florentina.grebe@gmx.net>
2013-04-13 22:20:35 +0200

    Changes to the HashWizard

--- org.jcryptool.visual.sig/src/org/jcryptool/visual/sig/ui/wizards/HashWizard.java
+++ org.jcryptool.visual.sig/src/org/jcryptool/visual/sig/ui/wizards/HashWizard.java
@@ -3,11 +3,20 @@ package org.jcryptool.visual.sig.ui.wizards;
 import org.eclipse.jface.wizard.Wizard;
 
 public class HashWizard extends Wizard{
+	HashWizardPage page;
+	private String name;
 
 	//Constructor
 	public HashWizard() {
 		super();
+		name = "test";
 	}	
+	
+	@Override
+	public void addPages() {
+		page = new HashWizardPage(name);
+		addPage(page);
+	}
 
 	@Override
 	public boolean performFinish() {
---
+++ org.jcryptool.visual.sig/src/org/jcryptool/visual/sig/ui/wizards/HashWizardPage.java
@@ -0,0 +1,21 @@
+package org.jcryptool.visual.sig.ui.wizards;
+
+import org.eclipse.jface.wizard.WizardPage;
+import org.eclipse.swt.widgets.Composite;
+
+public class HashWizardPage extends WizardPage {
+	private HashComposite composite;
+
+	protected HashWizardPage(String pageName) {
+		//TEST!
+		super("Hashwizard");
+		setDescription("YEESSSSS");
+	}
+	
+	public void createControl(Composite parent) {
+		composite = new HashComposite(parent, NONE);
+		setControl(composite);
+		setPageComplete(true);
+	}
+
+}
