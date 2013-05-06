ce8b59202712404c2f51d8eba1e5b4f438740b2e
Simon Hornbachner <yrxl45njid4w@nym.hush.com>
2013-05-07 00:22:14 +0200

    and more string magic…

--- org.jcryptool.visual.jctca/src/org/jcryptool/visual/jctca/listeners/PluginBtnListener.java
+++ org.jcryptool.visual.jctca/src/org/jcryptool/visual/jctca/listeners/PluginBtnListener.java
@@ -26,7 +26,6 @@ public class PluginBtnListener implements SelectionListener {
 		this.exp = exp;
 	}
 
-
 	@Override
 	public void widgetDefaultSelected(SelectionEvent arg0) {
 		// TODO Auto-generated method stub
@@ -38,35 +37,35 @@ public class PluginBtnListener implements SelectionListener {
 		Composite comp_image = lbl_img.getParent();
 		String text = ((Button) arg0.getSource()).getText();
 		ResizeHelper util = new ResizeHelper();
-		
+
 		if (text.equals(Messages.PluginBtnListener_strcmp_continue)) {
 			visual.disposeCompCenter();
 			visual.showCenter();
-			exp.setText("Erklaerunstext plugin allgemein");
+			exp.setText("Dieses Plugin stellt die grundlegende Funktionsweise einer Public Key Infrastructure (PKI) dar. Sie können ein Schlüsselpaar und eine dazugehörige Zertifikatsanfrage erstellen. Sie haben dann die Möglichkeit, den Weg zu verfolgen, den Ihre Anfrage durch die verschiedenen Ebenen der PKI durchläuft. Unter \"Registration Authority\" (RA) wird vermittelt, wie die Identitaetsprüfung ablaufen kann und worauf dabei zu achten ist. Unter \"Certification Authority\" (CA) können schließlich zu den zuvor erstellten und geprüften Anfragen Zertifikate ausgestellt werden. Nach Ausstellung eines Zertifikates haben Sie die Möglichkeit, Signaturen für Texte und Dateien zu erstellen und diese Signaturen zu ueberprüfen. Erstellte Zertifikate können außerdem in der Benutzer-Ansicht widerrufen werden. Jeder Schritt wird von Erklärungstexten begleitet, der Ihnen Informationen zu den jeweiligen Schritten liefert und auf eventuelle Fallstricke hinweist. Ausführliche Hintergrundinformationen zu PKIs und den hier dargestellten Prozessen finden Sie in der Onlinehilfe.");
 		} else if (text.equals(Messages.PluginBtnListener_strcmp_create_cert)) {
 			help = Activator.getImageDescriptor(
 					Messages.PluginBtnListener_create_cert_arch_path)
 					.createImage();
-			exp.setText("Erklaerungstext Create");
+			exp.setText("Möchte ein Benutzer beweisen, dass ein öffentlicher Schlüssel tatsächlich zu einem privaten Schlüssel in seinem Besitz gehört, hat er die Möglichkeit, sich für diesen Schlüssel ein Zertifikat ausstellen zu lassen. Zu diesem Zweck erstellt der Benutzer einen \"Certificate Signing Request\" (CSR) für seinen öffentlichen Schlüssel und leitet diesen zusammen mit einem Identitätsnachweis (beispielsweise einer Ausweiskopie) an eine sogenannte \"Registration Authority\" (RA) weiter. Die RA überprüft, ob es sich beim Antragsteller auch tatsächlich um die Person handelt, für die das Zertifikat ausgestellt werden soll und leitet den CSR, wenn dies der Fall ist, an eine \"Certification Authority\" (CA) weiter. Die CA fügt dem CSR ihre eigene Signatur hinzu, wodurch dieser zu einem von dieser CA ausgestellten Zertifikat wird. Dieses Zertifikat geht zurück an den Antragsteller. Die RA und die CA werden gemeinsam auch als \"Public Key Infrastructure\" (PKI) bezeichnet.");
 			util.resize_image(lbl_img, comp_image, help);
 			util.set_image_name("create");
 		} else if (text.equals(Messages.PluginBtnListener_strcmp_rev_cert)) {
 			help = Activator.getImageDescriptor(
 					Messages.PluginBtnListener_rev_cert_arch_path)
 					.createImage();
-			exp.setText("Erklaerungstext Revoke");
+			exp.setText("Es gibt mehrere Gründe, warum ein Zertifikat widerrufen werden muss, bevor es seine Gültigkeitsdauer überschritten hat, welche Sie im Detail in der Onlinehilfe nachlesen können. Will der Zertifikatsinhaber sein Zertifikat widerrufen, so muss er dafür einen \"Revocation Request\" (RR) erstellen. Dieser RR geht ähnlich wie ein CSR zunächst an die RA zur Prüfung. Erklärt die RA diesen Request als authentisch, so wird er an die CA weitergeleitet. Die CA verwaltet eine \"Certificate Revocation List\" (CRL), also eine Liste aller Zertifikate, die von ihr ausgestellt wurden und ihr Gültigkeitsdatum noch nicht überschritten haben, aber vom Zertifikatsinhaber als ungültig erklärt wurden. Wenn eine CA einen RR weitergeleitet bekommt, so fügt sie das betroffene Zertifikat dieser CRL hinzu.");
 			util.resize_image(lbl_img, comp_image, help);
 			util.set_image_name("revoke");
 		} else if (text.equals(Messages.PluginBtnListener_strcmp_check_sig)) {
 			help = Activator.getImageDescriptor(
 					Messages.PluginBtnListener_check_sig_arch_path)
 					.createImage();
-			exp.setText("Erklaerungstext Check");
+			exp.setText("Will der Empfänger signierter Daten, wie in diesem Beispiel einer E-Mail, die Echtheit dieser prüfen, so sind dafür im groben zwei Schritte notwendig. Zunächst muss überprüft werden, ob die Signatur der E-Mail tatsächlich vom privaten Schlüssel stammt, der zum Zertifikat des Empfängers gehört. Anschließend muss die CRL der ausstellenden CA angefordert und geprüft werden, ob das Zertifikat vom Inhaber widerrufen wurde. Befindet sich das Zertifikat auf der CRL, so ist die Signatur nur dann gültig, wenn das Datum der Signatur vor dem Datum des Widerrufs liegt.");
 			util.resize_image(lbl_img, comp_image, help);
 			util.set_image_name("check");
 
 		}
-		
+
 	}
 
 }
