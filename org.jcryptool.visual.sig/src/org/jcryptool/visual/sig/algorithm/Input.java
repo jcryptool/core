673efb4d6807207399076e38b8cd4ddd24de98cf
Florentina Grebe <florentina.grebe@gmx.net>
2013-05-14 15:55:49 +0300

    Update Input.java

--- org.jcryptool.visual.sig/src/org/jcryptool/visual/sig/algorithm/Input.java
+++ org.jcryptool.visual.sig/src/org/jcryptool/visual/sig/algorithm/Input.java
@@ -2,9 +2,10 @@ package org.jcryptool.visual.sig.algorithm;
 
 public class Input {
 	
-	public static byte[] data;
+	public static byte[] data; //Contains the input data
 	
-	public static String path = "";
-
+	public static String path = ""; //Contains the path to the input data
+	
+	public static byte[] hash; //Contains the hash of the input data
 
 }
