bfb1a528776886e5ce8317a6dcdd063fac0e83d7
Cornelia Gierlinger <s1110239011@students.fh-hagenberg.at>
2013-05-17 10:43:27 +0200

    Removed unused import.

--- org.jcryptool.visual.sig/src/org/jcryptool/visual/sig/algorithm/SigGeneration.java
+++ org.jcryptool.visual.sig/src/org/jcryptool/visual/sig/algorithm/SigGeneration.java
@@ -1,8 +1,6 @@
 package org.jcryptool.visual.sig.algorithm;
 
 import java.security.*;
-import javax.crypto.*;
-
 import org.jcryptool.core.logging.utils.LogUtil;
 import org.jcryptool.visual.sig.SigPlugin;
 
