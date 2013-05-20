f30f6151f980fd2be398c8e98b395e3c4270e376
Kerstin Reisinger <s1010239025@students.fh-hagenberg.at>
2013-05-20 16:56:33 +0200

    pubkeylistener for another gui change in the user tab

---
+++ org.jcryptool.visual.jctca/src/org/jcryptool/visual/jctca/listeners/PubKeyListener.java
@@ -0,0 +1,25 @@
+package org.jcryptool.visual.jctca.listeners;
+
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.widgets.Combo;
+
+public class PubKeyListener implements SelectionListener {
+Combo cmb_genKey;
+	
+	public PubKeyListener(Combo cmb_genKey) {
+		this.cmb_genKey = cmb_genKey;
+	}
+	@Override
+	public void widgetSelected(SelectionEvent e) {
+		cmb_genKey.setEnabled(true);
+
+	}
+
+	@Override
+	public void widgetDefaultSelected(SelectionEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
