e0b009d5aa9e735f2ce12fdb387e2cd296205d48
Cornelia Gierlinger <s1110239011@students.fh-hagenberg.at>
2013-06-05 11:43:54 +0200

    changed textbox to lable.

--- org.jcryptool.visual.sig/src/org/jcryptool/visual/sig/ui/wizards/ShowSig.java
+++ org.jcryptool.visual.sig/src/org/jcryptool/visual/sig/ui/wizards/ShowSig.java
@@ -5,7 +5,6 @@ import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Button;
-import org.eclipse.swt.widgets.Text;
 import org.eclipse.wb.swt.SWTResourceManager;
 
 import org.eclipse.swt.events.SelectionAdapter;
@@ -13,18 +12,19 @@ import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.TableColumn;
+import org.eclipse.swt.widgets.Label;
 
 public class ShowSig extends Shell {
-	private Text txtT;
-	private Text txtT_2;
-	private Text txtT_1;
-	private Text text;
-	private Text text_1;
-	private Text text_2;
-	private Text txtSig;
-	private Text txtLnge;
-	private Text txtSignedMes;
-	private Text txtLngeMes;
+	private Label txtT;
+	private Label txtT_2;
+	private Label txtT_1;
+	private Label text;
+	private Label text_1;
+	private Label text_2;
+	private Label txtSig;
+	private Label txtLnge;
+	private Label txtSignedMes;
+	private Label txtLngeMes;
 	private Table table;
 	private TableColumn tblclmnAdresse;
 	private TableColumn tblclmnHex;
@@ -44,17 +44,17 @@ public class ShowSig extends Shell {
 		Composite composite = new Composite(this, SWT.NONE);
 		composite.setBounds(10, 10, 440, 536);
 		
-		txtT = new Text(composite, SWT.READ_ONLY | SWT.WRAP);
+		txtT = new Label(composite, SWT.READ_ONLY | SWT.WRAP);
 		txtT.setFont(SWTResourceManager.getFont("Segoe UI", 9, SWT.BOLD));
 		txtT.setText(Messages.ShowSig_ownerTitle);
 		txtT.setBounds(0, 0, 137, 21);
 		
-		txtT_2 = new Text(composite, SWT.READ_ONLY);
+		txtT_2 = new Label(composite, SWT.READ_ONLY);
 		txtT_2.setFont(SWTResourceManager.getFont("Segoe UI", 9, SWT.BOLD));
 		txtT_2.setText(Messages.ShowSig_keyTitle);
 		txtT_2.setBounds(0, 25, 137, 21);
 		
-		txtT_1 = new Text(composite, SWT.READ_ONLY);
+		txtT_1 = new Label(composite, SWT.READ_ONLY);
 		txtT_1.setFont(SWTResourceManager.getFont("Segoe UI", 9, SWT.BOLD));
 		txtT_1.setText(Messages.ShowSig_methodTitle);
 		txtT_1.setBounds(0, 49, 137, 21);
@@ -63,16 +63,16 @@ public class ShowSig extends Shell {
 		
 		// get owner of the key
 		String userName = org.jcryptool.visual.sig.algorithm.Input.privateKey.getContactName();
-		text = new Text(composite, SWT.READ_ONLY | SWT.WRAP);
+		text = new Label(composite, SWT.READ_ONLY | SWT.WRAP);
 		text.setText(userName);
 		text.setBounds(143, 0, 287, 21);
 		
 		
-		text_1 = new Text(composite, SWT.READ_ONLY);
+		text_1 = new Label(composite, SWT.READ_ONLY);
 		text_1.setText(org.jcryptool.visual.sig.algorithm.Input.privateKey.getClassName());
 		text_1.setBounds(143, 25, 287, 21);
 		
-		text_2 = new Text(composite, SWT.READ_ONLY);
+		text_2 = new Label(composite, SWT.READ_ONLY);
 		text_2.setText(sig);
 		//text_2.setText("--> chooseSignature");
 		text_2.setBounds(143, 49, 287, 21);
@@ -98,49 +98,49 @@ public class ShowSig extends Shell {
 		btnNewButton_1.setText(Messages.ShowSig_btnSave);
 		btnNewButton_1.setEnabled(false);
 		
-		txtSig = new Text(composite, SWT.READ_ONLY);
+		txtSig = new Label(composite, SWT.READ_ONLY);
 		txtSig.setFont(SWTResourceManager.getFont("Segoe UI", 9, SWT.BOLD));
 		txtSig.setForeground(SWTResourceManager.getColor(0, 0, 0));
 		txtSig.setText(Messages.ShowSig_grpSignature);
 		txtSig.setBounds(0, 76, 137, 21);
 		
-		txtLnge = new Text(composite, SWT.READ_ONLY);
+		txtLnge = new Label(composite, SWT.READ_ONLY);
 		txtLnge.setText(Messages.ShowSig_lengthSig);
 		txtLnge.setBounds(0, 213, 200, 21);
 		
 		// TODO 
 		Group grpOption = new Group(composite, SWT.NONE);
 		grpOption.setText(Messages.ShowSig_grpOption);
-		grpOption.setBounds(0, 240, 440, 82);
+		grpOption.setBounds(0, 245, 440, 72);
 		grpOption.setLayout(null);
 		
 		Button btnZahl = new Button(grpOption, SWT.RADIO);
-		btnZahl.setBounds(10, 32, 113, 16);
+		btnZahl.setBounds(10, 43, 113, 16);
 		btnZahl.setText(Messages.ShowSig_number);
 		
 		Button btnOkt = new Button(grpOption, SWT.RADIO);
-		btnOkt.setBounds(129, 32, 90, 16);
+		btnOkt.setBounds(129, 43, 90, 16);
 		btnOkt.setText(Messages.ShowSig_octal);
 		
 		Button btnDez = new Button(grpOption, SWT.RADIO);
-		btnDez.setBounds(237, 32, 90, 16);
+		btnDez.setBounds(237, 43, 90, 16);
 		btnDez.setText(Messages.ShowSig_decimal);
 		
 		Button btnHex = new Button(grpOption, SWT.RADIO);
-		btnHex.setBounds(336, 32, 90, 16);
+		btnHex.setBounds(336, 43, 90, 16);
 		btnHex.setText(Messages.ShowSig_hex);
 		
 		Button btnHexdump = new Button(grpOption, SWT.RADIO);
 		btnHexdump.setSelection(true);
-		btnHexdump.setBounds(10, 10, 191, 16);
+		btnHexdump.setBounds(10, 21, 191, 16);
 		btnHexdump.setText(Messages.ShowSig_hexDump);
 		
-		txtSignedMes = new Text(composite, SWT.READ_ONLY);
+		txtSignedMes = new Label(composite, SWT.READ_ONLY);
 		txtSignedMes.setFont(SWTResourceManager.getFont("Segoe UI", 9, SWT.BOLD));
 		txtSignedMes.setText(Messages.ShowSig_grpMessage);
 		txtSignedMes.setBounds(0, 328, 137, 21);
 		
-		txtLngeMes = new Text(composite, SWT.READ_ONLY);
+		txtLngeMes = new Label(composite, SWT.READ_ONLY);
 		txtLngeMes.setText(Messages.ShowSig_lengthMessage);
 		txtLngeMes.setBounds(0, 465, 200, 21);
 		
