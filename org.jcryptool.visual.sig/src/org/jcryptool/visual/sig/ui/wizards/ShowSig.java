8bfdf2f3b3669386b79834f96fd73df983ec81bc
Cornelia Gierlinger <s1110239011@students.fh-hagenberg.at>
2013-06-05 23:23:18 +0200

    added SelectionEvent to radio button. set table invisible if a number reprepresentation button is selected.

--- org.jcryptool.visual.sig/src/org/jcryptool/visual/sig/ui/wizards/ShowSig.java
+++ org.jcryptool.visual.sig/src/org/jcryptool/visual/sig/ui/wizards/ShowSig.java
@@ -33,6 +33,7 @@ public class ShowSig extends Shell {
 	private TableColumn tableColumn_1;
 	private TableColumn tableColumn_2;
 	private Label lblNewLabel;
+	private Label lblSigNum;
 
 	/**
 	 * Create the shell.
@@ -73,6 +74,128 @@ public class ShowSig extends Shell {
 		text_2.setText(sig);
 		text_2.setBounds(143, 49, 287, 21);
 		
+		txtSig = new Label(composite, SWT.READ_ONLY);
+		txtSig.setText(Messages.ShowSig_grpSignature);
+		txtSig.setBounds(0, 76, 137, 21);
+		
+		txtLnge = new Label(composite, SWT.READ_ONLY);
+		txtLnge.setText(Messages.ShowSig_lengthSig);
+		txtLnge.setBounds(0, 213, 200, 21);
+		
+		// TODO 
+		Group grpOption = new Group(composite, SWT.NONE);
+		grpOption.setText(Messages.ShowSig_grpOption);
+		grpOption.setBounds(0, 245, 440, 93);
+		grpOption.setLayout(null);
+			
+		lblNewLabel = new Label(grpOption, SWT.NONE);
+		lblNewLabel.setBounds(10, 44, 162, 15);
+		lblNewLabel.setText(Messages.ShowSig_dispOpt);
+		
+		txtSignedMes = new Label(composite, SWT.READ_ONLY);
+		txtSignedMes.setText(Messages.ShowSig_grpMessage);
+		txtSignedMes.setBounds(0, 344, 137, 21);
+		
+		txtLngeMes = new Label(composite, SWT.READ_ONLY);
+		txtLngeMes.setText(Messages.ShowSig_lengthMessage);
+		txtLngeMes.setBounds(0, 481, 200, 21);
+		
+		
+
+		table = new Table(composite, SWT.BORDER | SWT.FULL_SELECTION);
+		table.setLinesVisible(true);
+		table.setHeaderVisible(true);
+		table.setBounds(0, 103, 440, 104);
+		
+		tblclmnAdresse = new TableColumn(table, SWT.NONE);
+		tblclmnAdresse.setWidth(100);
+		tblclmnAdresse.setToolTipText("");
+		tblclmnAdresse.setText("address");
+		
+		tblclmnHex = new TableColumn(table, SWT.NONE);
+		tblclmnHex.setWidth(224);
+		tblclmnHex.setText("hex");
+		
+		tblclmnAscii = new TableColumn(table, SWT.NONE);
+		tblclmnAscii.setWidth(112);
+		tblclmnAscii.setText("ascii");
+		
+		lblSigNum = new Label(composite, SWT.NONE);
+		lblSigNum.setBounds(0, 103, 440, 104);
+		
+		table_1 = new Table(composite, SWT.BORDER | SWT.FULL_SELECTION);
+		table_1.setLinesVisible(true);
+		table_1.setHeaderVisible(true);
+		table_1.setBounds(0, 371, 440, 104);
+		
+		tblclmnAddress = new TableColumn(table_1, SWT.NONE);
+		tblclmnAddress.setWidth(100);
+		tblclmnAddress.setToolTipText("");
+		tblclmnAddress.setText("address");
+		
+		tableColumn_1 = new TableColumn(table_1, SWT.NONE);
+		tableColumn_1.setWidth(224);
+		tableColumn_1.setText("hex");
+		
+		tableColumn_2 = new TableColumn(table_1, SWT.NONE);
+		tableColumn_2.setWidth(112);
+		tableColumn_2.setText("ascii");
+		
+		// Radiobuttons
+		
+		// OKT
+		Button btnOkt = new Button(grpOption, SWT.RADIO);
+		btnOkt.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				table.setVisible(false);
+				lblSigNum.setVisible(true);
+				lblSigNum.setText(org.jcryptool.visual.sig.algorithm.Input.hashHex);
+			}
+		});
+		btnOkt.setBounds(178, 43, 80, 16);
+		btnOkt.setText(Messages.ShowSig_octal);
+		
+		//DEZ
+		Button btnDez = new Button(grpOption, SWT.RADIO);
+		btnDez.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				table.setVisible(false);
+				lblSigNum.setVisible(true);
+				lblSigNum.setText(org.jcryptool.visual.sig.algorithm.Input.hashHex);
+			}
+		});
+		btnDez.setBounds(264, 43, 80, 16);
+		btnDez.setText(Messages.ShowSig_decimal);
+		
+		// HEX
+		Button btnHex = new Button(grpOption, SWT.RADIO);
+		btnHex.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				table.setVisible(false);
+				lblSigNum.setVisible(true);
+				lblSigNum.setText(org.jcryptool.visual.sig.algorithm.Input.hashHex);
+			}
+		});
+		btnHex.setBounds(350, 43, 80, 16);
+		btnHex.setText(Messages.ShowSig_hex);
+
+		
+		// DUMP
+		Button btnHexdump = new Button(grpOption, SWT.RADIO);
+		btnHexdump.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				lblSigNum.setVisible(false);
+			}
+		});
+		btnHexdump.setSelection(true);
+		btnHexdump.setBounds(10, 21, 191, 16);
+		btnHexdump.setText(Messages.ShowSig_hexDump);
+		
+		// Button Close and Save
 		Button btnNewButton = new Button(composite, SWT.NONE);
 		btnNewButton.addSelectionListener(new SelectionAdapter() {
 			@Override
@@ -94,86 +217,9 @@ public class ShowSig extends Shell {
 		btnNewButton_1.setText(Messages.ShowSig_btnSave);
 		btnNewButton_1.setEnabled(false);
 		
-		txtSig = new Label(composite, SWT.READ_ONLY);
-		txtSig.setText(Messages.ShowSig_grpSignature);
-		txtSig.setBounds(0, 76, 137, 21);
-		
-		txtLnge = new Label(composite, SWT.READ_ONLY);
-		txtLnge.setText(Messages.ShowSig_lengthSig);
-		txtLnge.setBounds(0, 213, 200, 21);
-		
-		// TODO 
-		Group grpOption = new Group(composite, SWT.NONE);
-		grpOption.setText(Messages.ShowSig_grpOption);
-		grpOption.setBounds(0, 245, 440, 93);
-		grpOption.setLayout(null);
-		
-		Button btnOkt = new Button(grpOption, SWT.RADIO);
-		btnOkt.setBounds(178, 43, 80, 16);
-		btnOkt.setText(Messages.ShowSig_octal);
-		
-		Button btnDez = new Button(grpOption, SWT.RADIO);
-		btnDez.setBounds(264, 43, 80, 16);
-		btnDez.setText(Messages.ShowSig_decimal);
-		
-		Button btnHex = new Button(grpOption, SWT.RADIO);
-		btnHex.setBounds(350, 43, 80, 16);
-		btnHex.setText(Messages.ShowSig_hex);
-		
-		Button btnHexdump = new Button(grpOption, SWT.RADIO);
-		btnHexdump.setSelection(true);
-		btnHexdump.setBounds(10, 21, 191, 16);
-		btnHexdump.setText(Messages.ShowSig_hexDump);
-		
-		lblNewLabel = new Label(grpOption, SWT.NONE);
-		lblNewLabel.setBounds(10, 44, 162, 15);
-		lblNewLabel.setText(Messages.ShowSig_dispOpt);
-		
-		txtSignedMes = new Label(composite, SWT.READ_ONLY);
-		txtSignedMes.setText(Messages.ShowSig_grpMessage);
-		txtSignedMes.setBounds(0, 344, 137, 21);
-		
-		txtLngeMes = new Label(composite, SWT.READ_ONLY);
-		txtLngeMes.setText(Messages.ShowSig_lengthMessage);
-		txtLngeMes.setBounds(0, 481, 200, 21);
-		
-		// TODO
-		table = new Table(composite, SWT.BORDER | SWT.FULL_SELECTION);
-		table.setBounds(0, 103, 440, 104);
-		table.setHeaderVisible(true);
-		table.setLinesVisible(true);
-		
-		tblclmnAdresse = new TableColumn(table, SWT.NONE);
-		tblclmnAdresse.setToolTipText("");
-		tblclmnAdresse.setWidth(100);
-		tblclmnAdresse.setText("address");
-		
-		tblclmnHex = new TableColumn(table, SWT.NONE);
-		tblclmnHex.setWidth(224);
-		tblclmnHex.setText("hex");
-		
-		tblclmnAscii = new TableColumn(table, SWT.NONE);
-		tblclmnAscii.setWidth(112);
-		tblclmnAscii.setText("ascii");
-		
-		table_1 = new Table(composite, SWT.BORDER | SWT.FULL_SELECTION);
-		table_1.setLinesVisible(true);
-		table_1.setHeaderVisible(true);
-		table_1.setBounds(0, 371, 440, 104);
-		
-		tblclmnAddress = new TableColumn(table_1, SWT.NONE);
-		tblclmnAddress.setWidth(100);
-		tblclmnAddress.setToolTipText("");
-		tblclmnAddress.setText("address");
-		
-		tableColumn_1 = new TableColumn(table_1, SWT.NONE);
-		tableColumn_1.setWidth(224);
-		tableColumn_1.setText("hex");
-		
-		tableColumn_2 = new TableColumn(table_1, SWT.NONE);
-		tableColumn_2.setWidth(112);
-		tableColumn_2.setText("ascii");
 		createContents();
+		
+		
 	}
 
 	/**
