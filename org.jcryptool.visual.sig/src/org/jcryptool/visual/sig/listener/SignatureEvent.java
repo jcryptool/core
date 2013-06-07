0aae31d2de5e1e5b00de5ef67da47b5491564250
Florentina Grebe <florentina.grebe@gmx.net>
2013-06-07 10:23:40 +0200

    Added SignatureEvent and SignatureListener

---
+++ org.jcryptool.visual.sig/src/org/jcryptool/visual/sig/listener/SignatureEvent.java
@@ -0,0 +1,151 @@
+package org.jcryptool.visual.sig.listener;
+
+import java.util.Date;
+
+import org.jcryptool.crypto.keystore.backend.KeyStoreAlias;
+
+public class SignatureEvent {
+
+	
+	/**
+	 * the signature from the sigvis plugin
+	 */
+	private byte[] signature;
+	
+	/**
+	 * path to the file that has been signed
+	 */
+	String path;
+	
+	/**
+	 * text that has been signed
+	 */
+	String text;
+	
+	/**
+	 * time when the signature was done
+	 */
+	Date time;
+	
+	/**
+	 * private key that has been used for the signature
+	 */
+	KeyStoreAlias privAlias;
+	
+	/**
+	 * public key
+	 */
+	KeyStoreAlias pubAlias;
+	
+	/**
+	 * 
+	 */
+	String hashalgorithm;
+	
+	/**
+	 * constructor for a signature
+	 * @param signature
+	 * @param path - path to the file that has been signed (text has to be null)
+	 * @param text - text that has been signed (path has to be null)
+	 * @param time - time when the signature was created
+	 * @param privAlias - alias for the private key that has been used for signing
+	 * @param pubAlias - alias for the corresponsing public key
+	 */
+	public SignatureEvent(byte[] signature, String path, String text, Date time, KeyStoreAlias privAlias, KeyStoreAlias pubAlias, String hashalgorithm){
+		this.signature = signature;
+		this.path = path;
+		this.text = text;
+		this.time = time;
+		this.privAlias = privAlias;
+		this.pubAlias = pubAlias;
+		this.hashalgorithm = hashalgorithm;
+	}
+	
+	public String getHashAlgorithm(){
+		return hashalgorithm;
+	}
+
+	/**
+	 * @return the signature
+	 */
+	public byte[] getSignature() {
+		return signature;
+	}
+
+	/**
+	 * @param signature the signature to set
+	 */
+	public void setSignature(byte[] signature) {
+		this.signature = signature;
+	}
+
+	/**
+	 * @return the path
+	 */
+	public String getPath() {
+		return path;
+	}
+
+	/**
+	 * @param path the path to set
+	 */
+	public void setPath(String path) {
+		this.path = path;
+	}
+
+	/**
+	 * @return the text
+	 */
+	public String getText() {
+		return text;
+	}
+
+	/**
+	 * @param text the text to set
+	 */
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	/**
+	 * @return the time
+	 */
+	public Date getTime() {
+		return time;
+	}
+
+	/**
+	 * @param time the time to set
+	 */
+	public void setTime(Date time) {
+		this.time = time;
+	}
+
+	/**
+	 * @return the alias
+	 */
+	public KeyStoreAlias getPrivAlias() {
+		return privAlias;
+	}
+
+	/**
+	 * @param privAlias the Alias to set
+	 */
+	public void setPrivAlias(KeyStoreAlias privAlias) {
+		this.privAlias = privAlias;
+	}
+	
+	/**
+	 * @return the alias
+	 */
+	public KeyStoreAlias getPubAlias() {
+		return pubAlias;
+	}
+
+	/**
+	 * @param pubAlias the Alias to set
+	 */
+	public void setPubAlias(KeyStoreAlias pubAlias) {
+		this.pubAlias = pubAlias;
+	}
+}
---
+++ org.jcryptool.visual.sig/src/org/jcryptool/visual/sig/listener/SignatureListener.java
@@ -0,0 +1,6 @@
+package org.jcryptool.visual.sig.listener;
+
+public interface SignatureListener {
+
+	void signaturePerformed(SignatureEvent e);
+}
