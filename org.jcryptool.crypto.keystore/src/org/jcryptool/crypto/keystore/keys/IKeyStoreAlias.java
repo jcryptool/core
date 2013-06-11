22381ff99c34cbb34f147ce0fc5bc415c16d46f3
Dominik Schadow <dominikschadow@googlemail.com>
2013-06-11 22:49:35 +0200

    Keystore refactoring
    Integrated org.jcryptool.crypto classes into keystore plug-in
    
    Signed-off-by: Dominik Schadow <dominikschadow@googlemail.com>

---
+++ org.jcryptool.crypto.keystore/src/org/jcryptool/crypto/keystore/keys/IKeyStoreAlias.java
@@ -0,0 +1,29 @@
+//-----BEGIN DISCLAIMER-----
+/*******************************************************************************
+* Copyright (c) 2013 JCrypTool Team and Contributors
+* 
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v1.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v10.html
+*******************************************************************************/
+//-----END DISCLAIMER-----
+package org.jcryptool.crypto.keystore.keys;
+
+public interface IKeyStoreAlias {
+   String getContactName();
+
+   String getEncodedContactName();
+
+   boolean isValid();
+
+   KeyType getKeyStoreEntryType();
+
+   String getOperation();
+
+   int getKeyLength();
+
+   String getHashValue();
+
+   String getAliasString();
+}
