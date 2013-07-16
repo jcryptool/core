42622ade6509471bd05f738052838438e53d631a
Dominik Schadow <dominikschadow@googlemail.com>
2013-07-16 22:15:51 +0200

    Added license information
    Signed-off-by: Dominik Schadow <dominikschadow@googlemail.com>

--- org.jcryptool.visual.jctca/src/org/jcryptool/visual/jctca/RegistrarViews/ShowCSR.java
+++ org.jcryptool.visual.jctca/src/org/jcryptool/visual/jctca/RegistrarViews/ShowCSR.java
@@ -1,3 +1,13 @@
+//-----BEGIN DISCLAIMER-----
+/*******************************************************************************
+* Copyright (c) 2013 JCrypTool Team and Contributors
+*
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v1.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v10.html
+*******************************************************************************/
+//-----END DISCLAIMER-----
 package org.jcryptool.visual.jctca.RegistrarViews;
 
 import java.util.Calendar;
