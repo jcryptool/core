ad52c996e0a161808105299de4bb33edc88ac03e
Matthias Walthart <matthias.walthart@db.com>
2013-07-31 16:49:54 +0200

    initial commit

---
+++ org.jcryptool.analysis.bruteforce/src/com/jcryptool/bruteforce/handlers/SampleHandler.java
@@ -0,0 +1,30 @@
+package com.jcryptool.bruteforce.handlers;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+import com.jcryptool.bruteforce.windows.GUI;
+
+/**
+ * Our sample handler extends AbstractHandler, an IHandler base class.
+ * @see org.eclipse.core.commands.IHandler
+ * @see org.eclipse.core.commands.AbstractHandler
+ */
+public class SampleHandler extends AbstractHandler {
+	/**
+	 * The constructor.
+	 */
+	public SampleHandler() {
+	}
+
+	/**
+	 * the command has been executed, so extract extract the needed information
+	 * from the application context.
+	 */
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		GUI window=new GUI();
+		window.setVisible(true);
+		return null;
+	}
+}
