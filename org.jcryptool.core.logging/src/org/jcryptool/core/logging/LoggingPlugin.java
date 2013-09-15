daaeccd09f4c98a12b3153534eec0ef0c61d109d
Dominik Schadow <dominikschadow@googlemail.com>
2013-09-15 13:59:08 +0200

    Fixed checkstyle warnings
    Signed-off-by: Dominik Schadow <dominikschadow@googlemail.com>

--- org.jcryptool.core.logging/src/org/jcryptool/core/logging/LoggingPlugin.java
+++ org.jcryptool.core.logging/src/org/jcryptool/core/logging/LoggingPlugin.java
@@ -1,3 +1,5 @@
+// -----BEGIN DISCLAIMER-----
+/*******************************************************************************
 //-----BEGIN DISCLAIMER-----
 /*******************************************************************************
 * Copyright (c) 2008 JCrypTool Team and Contributors
@@ -15,58 +17,60 @@ import org.jcryptool.core.logging.utils.LogUtil;
 import org.osgi.framework.BundleContext;
 
 /**
- * <p>JCrypTool logging plug-in. The logging is based on Eclipse logging, this
- * plug-in is only added for convenience. There is no need to instantiate this
- * plug-in directly, the <code>LogUtil</code> class provides only static methods
- * for easy access.</p>
- *
- * <p>The log messages are stored in the workbench log file (Eclipse takes care of
- * that). User access is possible via the <b>Error View</b> included in the JCrypTool
- * platform.</p>
- *
+ * <p>
+ * JCrypTool logging plug-in. The logging is based on Eclipse logging, this plug-in is only added for convenience. There
+ * is no need to instantiate this plug-in directly, the <code>LogUtil</code> class provides only static methods for easy
+ * access.
+ * </p>
+ * 
+ * <p>
+ * The log messages are stored in the workbench log file (Eclipse takes care of that). User access is possible via the
+ * <b>Error View</b> included in the JCrypTool platform.
+ * </p>
+ * 
  * @author Dominik Schadow
  * @version 0.5.0
  */
 public class LoggingPlugin extends AbstractUIPlugin {
-	/** The plug-in ID. */
-	public static final String PLUGIN_ID = "org.jcryptool.core.logging"; //$NON-NLS-1$
-	/** The shared instance. */
-	private static LoggingPlugin plugin;
+    /** The plug-in ID. */
+    public static final String PLUGIN_ID = "org.jcryptool.core.logging"; //$NON-NLS-1$
+    /** The shared instance. */
+    private static LoggingPlugin plugin;
 
-	/**
-	 * The constructor.
-	 */
-	public LoggingPlugin() {
-		plugin = this;
-	}
+    /**
+     * The constructor.
+     */
+    public LoggingPlugin() {
+        plugin = this;
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.core.runtime.Plugins#start(org.osgi.framework.BundleContext)
-	 */
-	public void start(BundleContext context) throws Exception {
-		super.start(context);
-		int loglevel = this.getPreferenceStore().getInt(LogUtil.LOGGER_LOG_LEVEL);
-        if (loglevel!=0){
-        	LogUtil.setLogLevel(loglevel);
+    /*
+     * (non-Javadoc)
+     * @see org.eclipse.core.runtime.Plugins#start(org.osgi.framework.BundleContext)
+     */
+    public void start(BundleContext context) throws Exception {
+        super.start(context);
+        int loglevel = this.getPreferenceStore().getInt(LogUtil.LOGGER_LOG_LEVEL);
+        if (loglevel != 0) {
+            LogUtil.setLogLevel(loglevel);
         }
-	}
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.core.runtime.Plugin#stop(org.osgi.framework.BundleContext)
-	 */
-	public void stop(BundleContext context) throws Exception {
-		plugin = null;
-		super.stop(context);
-	}
+    /*
+     * (non-Javadoc)
+     * @see org.eclipse.core.runtime.Plugin#stop(org.osgi.framework.BundleContext)
+     */
+    public void stop(BundleContext context) throws Exception {
+        plugin = null;
+        super.stop(context);
+    }
 
-	/**
-	 * Returns the shared instance
-	 *
-	 * @return the shared instance
-	 */
-	public static LoggingPlugin getDefault() {
-		return plugin;
-	}
+    /**
+     * Returns the shared instance.
+     * 
+     * @return the shared instance
+     */
+    public static LoggingPlugin getDefault() {
+        return plugin;
+    }
 }
--- org.jcryptool.core.logging/src/org/jcryptool/core/logging/dialogs/JCTMessageDialog.java
+++ org.jcryptool.core.logging/src/org/jcryptool/core/logging/dialogs/JCTMessageDialog.java
@@ -1,12 +1,13 @@
-// -----BEGIN DISCLAIMER-----
+//-----BEGIN DISCLAIMER-----
 /*******************************************************************************
- * Copyright (c) 2010 JCrypTool Team and Contributors
- *
- * All rights reserved. This program and the accompanying materials are made available under the terms of the Eclipse
- * Public License v1.0 which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *******************************************************************************/
-// -----END DISCLAIMER-----
+* Copyright (c) 2013 JCrypTool Team and Contributors
+*
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v1.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v10.html
+*******************************************************************************/
+//-----END DISCLAIMER-----
 package org.jcryptool.core.logging.dialogs;
 
 import org.eclipse.core.runtime.IStatus;
@@ -18,29 +19,34 @@ import org.eclipse.ui.PlatformUI;
 /**
  * JCrypTool message-handling plug-in. This class offers several convenience methods to display messages. All methods
  * are static for easy access.
- *
+ * 
  * @author Nils Reimers
  * @version 0.9.5
  */
 public class JCTMessageDialog {
     /**
+     * Utility class without public constructor.
+     */
+    private JCTMessageDialog() {
+    }
+
+    /**
      * Opens an error dialog to display the given error.
-     *
+     * 
      * @param error to display
      */
     public static void showErrorDialog(final Status error, final String message) {
-    	Display.getDefault().asyncExec(new Runnable() {
-    	    public void run() {
-    	    	ExceptionDetailsErrorDialog.openError(null,
-    	                Messages.MessageDialog_error, message, error);
-    	    }
-    	});
-        
+        Display.getDefault().asyncExec(new Runnable() {
+            public void run() {
+                ExceptionDetailsErrorDialog.openError(null, Messages.MessageDialog_error, message, error);
+            }
+        });
+
     }
 
     /**
      * Opens an info dialog to display the given info.
-     *
+     * 
      * @param info to display
      */
     public static void showInfoDialog(IStatus info) {
