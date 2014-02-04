23af66bcc2b339612311f4622cbe7cf2c0312902
Florian <florian@boehl.name>
2014-02-04 22:13:11 +0100

    Zudoku: Add license.

--- org.jcryptool.games.zudoku/src/edu/kit/iks/zudoku/VerificationPanel.java
+++ org.jcryptool.games.zudoku/src/edu/kit/iks/zudoku/VerificationPanel.java
@@ -1,3 +1,13 @@
+//-----BEGIN DISCLAIMER-----
+/*******************************************************************************
+* Copyright (c) 2013 Florian Böhl <florian@boehl.name>
+* 
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v1.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v10.html
+*******************************************************************************/
+//-----END DISCLAIMER-----
 package edu.kit.iks.zudoku;
 
 import java.awt.BorderLayout;
--- org.jcryptool.games.zudoku/src/org/jcryptool/games/zudoku/ZudokuPlugin.java
+++ org.jcryptool.games.zudoku/src/org/jcryptool/games/zudoku/ZudokuPlugin.java
@@ -1,12 +1,13 @@
-// -----BEGIN DISCLAIMER-----
+//-----BEGIN DISCLAIMER-----
 /*******************************************************************************
- * Copyright (c) 2011 JCrypTool Team and Contributors
- *
- * All rights reserved. This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License v1.0 which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *******************************************************************************/
-// -----END DISCLAIMER-----
+* Copyright (c) 2013 Florian Böhl <florian@boehl.name>
+* 
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v1.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v10.html
+*******************************************************************************/
+//-----END DISCLAIMER-----
 package org.jcryptool.games.zudoku;
 
 import org.eclipse.ui.plugin.AbstractUIPlugin;
