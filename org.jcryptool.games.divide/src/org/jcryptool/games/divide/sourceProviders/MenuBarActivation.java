93f32aeb83b74228d28e38fc3de2938f32a05312
Dominik Schadow <dominikschadow@googlemail.com>
2014-04-05 20:05:44 +0200

    private constructors in utility classes
    Signed-off-by: Dominik Schadow <dominikschadow@googlemail.com>

--- org.jcryptool.games.divide/src/org/jcryptool/games/divide/sourceProviders/MenuBarActivation.java
+++ org.jcryptool.games.divide/src/org/jcryptool/games/divide/sourceProviders/MenuBarActivation.java
@@ -1,7 +1,7 @@
 // -----BEGIN DISCLAIMER-----
 /*******************************************************************************
  * Copyright (c) 2013 JCrypTool team and contributors
- * 
+ *
  * All rights reserved. This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
@@ -13,6 +13,8 @@ import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.services.ISourceProviderService;
 
 public class MenuBarActivation {
+    private MenuBarActivation() {
+    }
 
     public static void enableNewGameState(boolean isEnabled) {
         ISourceProviderService service = (ISourceProviderService) PlatformUI.getWorkbench().getActiveWorkbenchWindow()
