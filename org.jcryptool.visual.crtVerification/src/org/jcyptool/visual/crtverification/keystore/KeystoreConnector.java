20ea9145142e6069b1733c7d2a8fb756032d8825
Clemens Elst <clemens.elst@hagenbergerkreis.at>
2014-05-13 23:15:39 +0200

    foo

--- org.jcryptool.visual.crtVerification/src/org/jcyptool/visual/crtverification/keystore/KeystoreConnector.java
+++ org.jcryptool.visual.crtVerification/src/org/jcyptool/visual/crtverification/keystore/KeystoreConnector.java
@@ -1,5 +1,12 @@
 package org.jcyptool.visual.crtverification.keystore;
 
+
+/**
+ * Connector to the JCT Keystore to load and store Certificates and Certificate Chains
+ * 
+ * @author Clemens Elst
+ *
+ */
 public class KeystoreConnector {
 
 }
