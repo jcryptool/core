aa0b69f75da42a1ed5159dc4ffa484e1dac73b35
grease082 <christophsee@localhost>
2014-05-31 11:20:14 +0200

    changed table ChooseCertView to list

--- org.jcryptool.visual.crtVerification/src/org/jcryptool/visual/crtverification/views/ChooseCertComposite.java
+++ org.jcryptool.visual.crtVerification/src/org/jcryptool/visual/crtverification/views/ChooseCertComposite.java
@@ -1,23 +1,16 @@
 package org.jcryptool.visual.crtverification.views;
 
-import org.eclipse.jface.window.Window;
-import org.eclipse.jface.wizard.WizardDialog;
+import org.eclipse.jface.viewers.ListViewer;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Shell;
-import org.eclipse.swt.widgets.Table;
-import org.eclipse.swt.widgets.TableItem;
-import org.eclipse.ui.part.Page;
-import org.jcryptool.core.logging.utils.LogUtil;
-import org.jcryptool.visual.crtverification.Activator;
+import org.eclipse.swt.widgets.List;
+import org.jcryptool.visual.crtverification.keystore.KeystoreConnector;
 
 public class ChooseCertComposite extends Composite {
-	private Table table;
-    private ChooseCertPage page;
+	private Composite parent;
+	private ChooseCertPage page;
 
     /**
      * Create the composite.
@@ -26,31 +19,26 @@ public class ChooseCertComposite extends Composite {
      */
     public ChooseCertComposite(Composite parent, int style, ChooseCertPage p) {
         super(parent, style);
-        page = p;
+        this.parent = parent;
+        this.page = p;
         
-        Button btnLoad = new Button(this, SWT.NONE);
-  
-        btnLoad.setBounds(346, 169, 94, 28);
-        btnLoad.setText("Load");
-        btnLoad.addSelectionListener(new SelectionAdapter() {
-            @Override
-            public void widgetSelected(SelectionEvent e) {
-                try {
-                    page.setPageComplete(true);
-                } catch (Exception ex) {
-                    LogUtil.logError(Activator.PLUGIN_ID, ex);
-                }
-            }
+        ListViewer listViewer = new ListViewer(this, SWT.BORDER | SWT.V_SCROLL);
+        List list = listViewer.getList();
+        list.addSelectionListener(new SelectionAdapter() {
+        	@Override
+        	public void widgetSelected(SelectionEvent e) {
+        		page.setPageComplete(true);
+        	}
         });
         
-        table = new Table(this, SWT.BORDER | SWT.FULL_SELECTION);
-        table.setLinesVisible(true);
-        table.setBounds(10, 10, 430, 153);
-        
-        TableItem tableItem = new TableItem(table, SWT.NONE);
-        tableItem.setText("Certificate");
-        tableItem.setChecked(false);
-        tableItem.setText(new String[] {});
+       
+        list.setBounds(10, 10, 430, 280);
+        KeystoreConnector ksc = new KeystoreConnector(); 
+        for(int i=0;i<ksc.getAllCertificates().size();i++){
+        	list.add(ksc.getAllCertificates().get(i).getSubjectDN().toString());
+        }
+       
+        // Example End
         
     }
 }
