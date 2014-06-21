f3bf1350cd19509436dbb036e9208366cbfe791f
Dominik Schadow <dominikschadow@googlemail.com>
2014-06-21 21:03:56 +0200

    Added ColorService to utils plug-in
    Signed-off-by: Dominik Schadow <dominikschadow@googlemail.com>

---
+++ org.jcryptool.core.util/src/org/jcryptool/core/util/colors/ColorService.java
@@ -0,0 +1,35 @@
+//-----BEGIN DISCLAIMER-----
+/*******************************************************************************
+* Copyright (c) 2014 JCrypTool Team and Contributors
+*
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v1.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v10.html
+*******************************************************************************/
+//-----END DISCLAIMER-----
+package org.jcryptool.core.util.colors;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.widgets.Display;
+
+/**
+ * Utility class for all colors used in JCrypTool. Plug-ins should use this class for all color purposes rather
+ * than instantiating a color themselves. You do not have to dispose any of these colors after usage.
+ *
+ * @author Dominik Schadow
+ * @version 1.0.0
+ */
+public class ColorService {
+    /** Colors for backgrounds. */
+    public static final Color WHITE, GREEN, RED, LIGHTGRAY, GRAY;
+
+    static {
+        WHITE = Display.getDefault().getSystemColor(SWT.COLOR_WHITE);
+        GRAY = Display.getDefault().getSystemColor(SWT.COLOR_DARK_GRAY);
+        RED = Display.getDefault().getSystemColor(SWT.COLOR_RED);
+        GREEN = Display.getDefault().getSystemColor(SWT.COLOR_GREEN);
+        LIGHTGRAY = Display.getDefault().getSystemColor(SWT.COLOR_WIDGET_BACKGROUND);
+    }
+}
