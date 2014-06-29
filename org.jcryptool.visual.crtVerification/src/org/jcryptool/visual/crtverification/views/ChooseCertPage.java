de98c15b73386068718988366a586ee27bf87bc8
grease082 <christophsee@localhost>
2014-06-29 17:08:25 +0200

    Revert "Load Button removed from ChooseCert"
    This reverts commit fc24403349dd81de11d7e4245bce05af16dbff42.

--- org.jcryptool.visual.crtVerification/src/org/jcryptool/visual/crtverification/views/ChooseCertPage.java
+++ org.jcryptool.visual.crtVerification/src/org/jcryptool/visual/crtverification/views/ChooseCertPage.java
@@ -7,7 +7,6 @@ import org.eclipse.swt.widgets.Composite;
 public class ChooseCertPage extends WizardPage {
    	private ChooseCertComposite compositeFile;
    	private CrtVerViewController controller;
-   	String selectedListItem;
     int certType; // [1] UserCert; [2] Cert; [3] RootCert
     
     public int getCertType() {
@@ -31,7 +30,6 @@ public class ChooseCertPage extends WizardPage {
     public void createControl(Composite parent) {
         setPageComplete(false);
         compositeFile = new ChooseCertComposite(parent, SWT.NONE, this, controller);
-        selectedListItem = compositeFile.contact_name;
         setControl(compositeFile);
     }
 
