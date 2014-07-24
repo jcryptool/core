c4e7c27a4b43d3243aa1270209312043d8d49d4c
Dominik Schadow <dominik.schadow@googlemail.com>
2014-07-24 20:45:16 +0200

    Removed unused code from plug-in start class
    Signed-off-by: Dominik Schadow <dominik.schadow@googlemail.com>

--- org.jcryptool.analysis.vigenere/src/org/jcryptool/analysis/vigenere/VigenereBreakerPlugin.java
+++ org.jcryptool.analysis.vigenere/src/org/jcryptool/analysis/vigenere/VigenereBreakerPlugin.java
@@ -11,62 +11,21 @@ package org.jcryptool.analysis.vigenere;
 
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
-import org.osgi.framework.BundleContext;
 
 /**
  * The activator class controls the plug-in life cycle
  */
 public class VigenereBreakerPlugin extends AbstractUIPlugin {
+    /** The plug-in ID. */
+    public static final String PLUGIN_ID = "org.jcryptool.analysis.vigenere"; //$NON-NLS-1$
 
-	// The plug-in ID
-	public static final String PLUGIN_ID = "org.jcryptool.analysis.vigenere"; //$NON-NLS-1$
-
-	// The shared instance
-	private static VigenereBreakerPlugin plugin;
-
-	/**
-	 * The constructor
-	 */
-	public VigenereBreakerPlugin() {
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
-	 */
-	@Override
-	public void start(BundleContext context) throws Exception {
-		super.start(context);
-		plugin = this;
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
-	 */
-	@Override
-	public void stop(BundleContext context) throws Exception {
-		plugin = null;
-		super.stop(context);
-	}
-
-	/**
-	 * Returns the shared instance
-	 *
-	 * @return the shared instance
-	 */
-	public static VigenereBreakerPlugin getDefault() {
-		return plugin;
-	}
-
-	/**
-	 * Returns an image descriptor for the image file at the given
-	 * plug-in relative path
-	 *
-	 * @param path the path
-	 * @return the image descriptor
-	 */
-	public static ImageDescriptor getImageDescriptor(String path) {
-		return imageDescriptorFromPlugin(PLUGIN_ID, path);
-	}
+    /**
+     * Returns an image descriptor for the image file at the given plug-in relative path
+     *
+     * @param path the path
+     * @return the image descriptor
+     */
+    public static ImageDescriptor getImageDescriptor(String path) {
+        return imageDescriptorFromPlugin(PLUGIN_ID, path);
+    }
 }
