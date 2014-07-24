4f49c92b6dd1f68f072d7ff766f224e6f5467397
Dominik Schadow <dominik.schadow@googlemail.com>
2014-07-24 20:49:24 +0200

    Removed unused code from plug-in start class
    Signed-off-by: Dominik Schadow <dominik.schadow@googlemail.com>

--- org.jcryptool.crypto.classic.adfgvx/src/org/jcryptool/crypto/classic/adfgvx/AdfgvxPlugin.java
+++ org.jcryptool.crypto.classic.adfgvx/src/org/jcryptool/crypto/classic/adfgvx/AdfgvxPlugin.java
@@ -11,51 +11,11 @@
 package org.jcryptool.crypto.classic.adfgvx;
 
 import org.eclipse.ui.plugin.AbstractUIPlugin;
-import org.osgi.framework.BundleContext;
 
 /**
  * The activator class controls the plug-in life cycle
  */
 public class AdfgvxPlugin extends AbstractUIPlugin {	
-	// The plug-in ID
+	/** The plug-in ID. */
 	public static final String PLUGIN_ID = "org.jcryptool.crypto.classic.adfgvx"; //$NON-NLS-1$
-
-	// The shared instance
-	private static AdfgvxPlugin plugin;
-
-	/**
-	 * The constructor
-	 */
-	public AdfgvxPlugin() {
-		plugin = this;
-	}
-	
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
-	 */
-	@Override
-	public void start(BundleContext context) throws Exception {
-		super.start(context);
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
-	 */
-	@Override
-	public void stop(BundleContext context) throws Exception {
-		plugin = null;
-		super.stop(context);
-	}
-
-	/**
-	 * Returns the shared instance
-	 *
-	 * @return the shared instance
-	 */
-	public static AdfgvxPlugin getDefault() {
-		return plugin;
-	}
-
 }
