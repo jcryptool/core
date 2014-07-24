f1e9b8b9ee9c99cf51b72e4ba6a430784d8a5f46
Dominik Schadow <dominik.schadow@googlemail.com>
2014-07-24 20:58:41 +0200

    Removed unused code from plug-in start class
    Signed-off-by: Dominik Schadow <dominik.schadow@googlemail.com>

--- org.jcryptool.crypto.keystore/src/org/jcryptool/crypto/keystore/KeyStorePlugin.java
+++ org.jcryptool.crypto.keystore/src/org/jcryptool/crypto/keystore/KeyStorePlugin.java
@@ -12,10 +12,10 @@ package org.jcryptool.crypto.keystore;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.jcryptool.crypto.keystore.backend.KeyStoreManager;
-import org.osgi.framework.BundleContext;
 
 /**
- * The activator class controls the plug-in life cycle and initializes the singleton JCrypTool KeyStoreManager.
+ * The activator class controls the plug-in life cycle and initializes the singleton JCrypTool
+ * KeyStoreManager.
  * 
  * @see org.jcryptool.crypto.keystore.backend.KeyStoreManager
  * 
@@ -24,41 +24,12 @@ import org.osgi.framework.BundleContext;
 public class KeyStorePlugin extends AbstractUIPlugin {
     /** The plug-in ID, value is {@value} . */
     public static final String PLUGIN_ID = "org.jcryptool.crypto.keystore"; //$NON-NLS-1$
-    /** The shared instance. */
-    private static KeyStorePlugin plugin;
 
     /**
      * The constructor, initializes the singleton KeyStoreManager instance.
      */
     public KeyStorePlugin() {
         KeyStoreManager.getInstance();
-    }
-
-    /*
-     * (non-Javadoc)
-     * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext )
-     */
-    public void start(BundleContext context) throws Exception {
-        super.start(context);
-        plugin = this;
-    }
-
-    /*
-     * (non-Javadoc)
-     * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext )
-     */
-    public void stop(BundleContext context) throws Exception {
-        plugin = null;
-        super.stop(context);
-    }
-
-    /**
-     * Returns the shared instance
-     * 
-     * @return the shared instance
-     */
-    public static KeyStorePlugin getDefault() {
-        return plugin;
     }
 
     /**
