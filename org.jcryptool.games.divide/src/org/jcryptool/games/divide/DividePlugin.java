96bb2c8d2d42a56aea10a21b94350447361414ca
Dominik Schadow <dominik.schadow@googlemail.com>
2014-07-24 21:00:24 +0200

    Removed unused code from plug-in start class
    Signed-off-by: Dominik Schadow <dominik.schadow@googlemail.com>

--- org.jcryptool.games.divide/src/org/jcryptool/games/divide/DividePlugin.java
+++ org.jcryptool.games.divide/src/org/jcryptool/games/divide/DividePlugin.java
@@ -10,51 +10,11 @@
 package org.jcryptool.games.divide;
 
 import org.eclipse.ui.plugin.AbstractUIPlugin;
-import org.osgi.framework.BundleContext;
 
 /**
  * The activator class controls the plug-in life cycle
  */
 public class DividePlugin extends AbstractUIPlugin {
-
-    // The plug-in ID
+    /** The plug-in ID. */
     public static final String PLUGIN_ID = "org.jcryptool.games.divide"; //$NON-NLS-1$
-
-    // The shared instance
-    private static DividePlugin plugin;
-
-    /**
-     * The constructor
-     */
-    public DividePlugin() {
-        super();
-    }
-
-    /*
-     * (non-Javadoc)
-     * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext )
-     */
-    public void start(BundleContext context) throws Exception {
-        super.start(context);
-        plugin = this;
-    }
-
-    /*
-     * (non-Javadoc)
-     * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext )
-     */
-    public void stop(BundleContext context) throws Exception {
-        plugin = null;
-        super.stop(context);
-    }
-
-    /**
-     * Returns the shared instance
-     * 
-     * @return the shared instance
-     */
-    public static DividePlugin getDefault() {
-        return plugin;
-    }
-
 }
