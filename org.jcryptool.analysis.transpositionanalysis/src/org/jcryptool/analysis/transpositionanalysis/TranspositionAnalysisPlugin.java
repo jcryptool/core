08b04af247e63b3da128a24f0313293e2687e64c
Dominik Schadow <dominik.schadow@googlemail.com>
2014-07-24 20:43:31 +0200

    Removed unused code from plug-in start class
    Signed-off-by: Dominik Schadow <dominik.schadow@googlemail.com>

--- org.jcryptool.analysis.transpositionanalysis/src/org/jcryptool/analysis/transpositionanalysis/TranspositionAnalysisPlugin.java
+++ org.jcryptool.analysis.transpositionanalysis/src/org/jcryptool/analysis/transpositionanalysis/TranspositionAnalysisPlugin.java
@@ -1,93 +1,40 @@
-//-----BEGIN DISCLAIMER-----
+// -----BEGIN DISCLAIMER-----
 /*******************************************************************************
  * Copyright (c) 2010 JCrypTool Team and Contributors
  *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
+ * All rights reserved. This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *******************************************************************************/
-//-----END DISCLAIMER-----
+// -----END DISCLAIMER-----
 package org.jcryptool.analysis.transpositionanalysis;
 
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.jface.resource.ImageRegistry;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
-import org.osgi.framework.BundleContext;
 
 /**
  * The activator class controls the plug-in life cycle
  */
 public class TranspositionAnalysisPlugin extends AbstractUIPlugin {
+    /** The plug-in ID. */
+    public static final String PLUGIN_ID = "org.jcryptool.analysis.transpositionanalysis"; //$NON-NLS-1$
 
-	public static final String FILE_INPUT_ICON = "fileInputIcon";
+    /**
+     * Returns an image descriptor for the image file at the given plug-in relative path
+     * 
+     * @param path the path
+     * @return the image descriptor
+     */
+    public static ImageDescriptor getImageDescriptor(String path) {
+        return imageDescriptorFromPlugin(PLUGIN_ID, path);
+    }
 
-	public static final String KEYBOARD_INPUT_ICON = "keyboardInputIcon";
-
-	// The plug-in ID
-	public static final String PLUGIN_ID = "org.jcryptool.analysis.transpositionanalysis"; //$NON-NLS-1$
-
-	// The shared instance
-	private static TranspositionAnalysisPlugin plugin;
-
-	/**
-	 * The constructor
-	 */
-	public TranspositionAnalysisPlugin() {
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext
-	 * )
-	 */
-	@Override
-	public void start(BundleContext context) throws Exception {
-		super.start(context);
-		plugin = this;
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext
-	 * )
-	 */
-	@Override
-	public void stop(BundleContext context) throws Exception {
-		plugin = null;
-		super.stop(context);
-	}
-
-	/**
-	 * Returns the shared instance
-	 * 
-	 * @return the shared instance
-	 */
-	public static TranspositionAnalysisPlugin getDefault() {
-		return plugin;
-	}
-
-	/**
-	 * Returns an image descriptor for the image file at the given plug-in
-	 * relative path
-	 * 
-	 * @param path
-	 *            the path
-	 * @return the image descriptor
-	 */
-	public static ImageDescriptor getImageDescriptor(String path) {
-		return imageDescriptorFromPlugin(PLUGIN_ID, path);
-	}
-	
-	@Override
-	protected void initializeImageRegistry(ImageRegistry reg) {
-		ImageDescriptor imageDescriptorKeys = getImageDescriptor("icons/keys.png");
-		reg.put(KEYBOARD_INPUT_ICON, imageDescriptorKeys.createImage());
-		ImageDescriptor imageDescriptorFile = getImageDescriptor("icons/file.png");
-		reg.put(FILE_INPUT_ICON, imageDescriptorFile.createImage());
-	}
+    @Override
+    protected void initializeImageRegistry(ImageRegistry reg) {
+        ImageDescriptor imageDescriptorKeys = getImageDescriptor("icons/keys.png");
+        reg.put("keyboardInputIcon", imageDescriptorKeys.createImage());
+        ImageDescriptor imageDescriptorFile = getImageDescriptor("icons/file.png");
+        reg.put("fileInputIcon", imageDescriptorFile.createImage());
+    }
 }
