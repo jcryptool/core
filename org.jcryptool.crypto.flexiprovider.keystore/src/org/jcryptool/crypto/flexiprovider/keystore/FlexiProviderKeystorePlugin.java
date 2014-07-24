3593be7f3ef2c364e626ed8c45751103f93050e5
Dominik Schadow <dominik.schadow@googlemail.com>
2014-07-24 20:58:12 +0200

    Removed unused code from plug-in start class
    Signed-off-by: Dominik Schadow <dominik.schadow@googlemail.com>

--- org.jcryptool.crypto.flexiprovider.keystore/src/org/jcryptool/crypto/flexiprovider/keystore/FlexiProviderKeystorePlugin.java
+++ org.jcryptool.crypto.flexiprovider.keystore/src/org/jcryptool/crypto/flexiprovider/keystore/FlexiProviderKeystorePlugin.java
@@ -2,9 +2,8 @@
 /*******************************************************************************
  * Copyright (c) 2008 JCrypTool Team and Contributors
  * 
- * All rights reserved. This program and the accompanying materials are made
- * available under the terms of the Eclipse Public License v1.0 which
- * accompanies this distribution, and is available at
+ * All rights reserved. This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *******************************************************************************/
 // -----END DISCLAIMER-----
@@ -12,7 +11,6 @@ package org.jcryptool.crypto.flexiprovider.keystore;
 
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
-import org.osgi.framework.BundleContext;
 
 import de.flexiprovider.api.Registry;
 import de.flexiprovider.api.SecureRandom;
@@ -22,52 +20,11 @@ import de.flexiprovider.api.exceptions.NoSuchAlgorithmException;
  * The activator class controls the plug-in life cycle
  */
 public class FlexiProviderKeystorePlugin extends AbstractUIPlugin {
-    // The plug-in ID
+    /** The plug-in ID. */
     public static final String PLUGIN_ID = "org.jcryptool.crypto.flexiprovider.keystore"; //$NON-NLS-1$
 
-    // The shared instance
-    private static FlexiProviderKeystorePlugin plugin;
-
     /**
-     * The constructor
-     */
-    public FlexiProviderKeystorePlugin() {
-    }
-
-    /*
-     * (non-Javadoc)
-     * 
-     * @see
-     * org.eclipse.core.runtime.Plugins#start(org.osgi.framework.BundleContext)
-     */
-    public void start(BundleContext context) throws Exception {
-        super.start(context);
-        plugin = this;
-    }
-
-    /*
-     * (non-Javadoc)
-     * 
-     * @see
-     * org.eclipse.core.runtime.Plugin#stop(org.osgi.framework.BundleContext)
-     */
-    public void stop(BundleContext context) throws Exception {
-        plugin = null;
-        super.stop(context);
-    }
-
-    /**
-     * Returns the shared instance
-     * 
-     * @return the shared instance
-     */
-    public static FlexiProviderKeystorePlugin getDefault() {
-        return plugin;
-    }
-
-    /**
-     * Returns an image descriptor for the image file at the given plug-in
-     * relative path
+     * Returns an image descriptor for the image file at the given plug-in relative path
      * 
      * @param path the path
      * @return the image descriptor
@@ -87,5 +44,4 @@ public class FlexiProviderKeystorePlugin extends AbstractUIPlugin {
             return null;
         }
     }
-
 }
