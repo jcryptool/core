379d278cdabdb149438d4642290f7a068d0bbae8
Dominik Schadow <dominik.schadow@googlemail.com>
2014-07-24 20:46:33 +0200

    Removed unused code from plug-in start class
    Signed-off-by: Dominik Schadow <dominik.schadow@googlemail.com>

--- org.jcryptool.analysis.viterbi/src/org/jcryptool/analysis/viterbi/ViterbiPlugin.java
+++ org.jcryptool.analysis.viterbi/src/org/jcryptool/analysis/viterbi/ViterbiPlugin.java
@@ -1,25 +1,24 @@
-//-----BEGIN DISCLAIMER-----
+// -----BEGIN DISCLAIMER-----
 /*******************************************************************************
-* Copyright (c) 2010 JCrypTool Team and Contributors
-*
-* All rights reserved. This program and the accompanying materials
-* are made available under the terms of the Eclipse Public License v1.0
-* which accompanies this distribution, and is available at
-* http://www.eclipse.org/legal/epl-v10.html
-*******************************************************************************/
-//-----END DISCLAIMER-----
+ * Copyright (c) 2010 JCrypTool Team and Contributors
+ *
+ * All rights reserved. This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+// -----END DISCLAIMER-----
 package org.jcryptool.analysis.viterbi;
 
 import org.eclipse.ui.plugin.AbstractUIPlugin;
-
+import org.osgi.framework.BundleContext;
 
 /**
  * The activator class controls the plug-in life cycle
  */
 public class ViterbiPlugin extends AbstractUIPlugin {
 
-	// The plug-in ID
-	public static final String PLUGIN_ID = "org.jcryptool.analysis.viterbi";
+    // The plug-in ID
+    public static final String PLUGIN_ID = "org.jcryptool.analysis.viterbi";
 
     /**
      * The shared instance
@@ -30,9 +29,25 @@ public class ViterbiPlugin extends AbstractUIPlugin {
      * The constructor
      */
     public ViterbiPlugin() {
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+     */
+    public void start(BundleContext context) throws Exception {
+        super.start(context);
         plugin = this;
     }
 
+    /*
+     * (non-Javadoc)
+     * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+     */
+    public void stop(BundleContext context) throws Exception {
+        plugin = null;
+        super.stop(context);
+    }
 
     /**
      * Returns the shared instance
