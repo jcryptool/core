70d0a678585d34b47530149b3c64843d8d75df77
Dominik Schadow <dominik.schadow@googlemail.com>
2014-07-24 20:56:18 +0200

    Removed unused code from plug-in start class
    Signed-off-by: Dominik Schadow <dominik.schadow@googlemail.com>

--- org.jcryptool.crypto.classic.vigenere/src/org/jcryptool/crypto/classic/vigenere/VigenerePlugin.java
+++ org.jcryptool.crypto.classic.vigenere/src/org/jcryptool/crypto/classic/vigenere/VigenerePlugin.java
@@ -1,17 +1,15 @@
-//-----BEGIN DISCLAIMER-----
+// -----BEGIN DISCLAIMER-----
 /*******************************************************************************
-* Copyright (c) 2008 JCrypTool Team and Contributors
-* 
-* All rights reserved. This program and the accompanying materials
-* are made available under the terms of the Eclipse Public License v1.0
-* which accompanies this distribution, and is available at
-* http://www.eclipse.org/legal/epl-v10.html
-*******************************************************************************/
-//-----END DISCLAIMER-----
+ * Copyright (c) 2008 JCrypTool Team and Contributors
+ * 
+ * All rights reserved. This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+// -----END DISCLAIMER-----
 package org.jcryptool.crypto.classic.vigenere;
 
 import org.eclipse.ui.plugin.AbstractUIPlugin;
-import org.osgi.framework.BundleContext;
 
 /**
  * The Activator class controls the plug-in life cycle
@@ -20,49 +18,6 @@ import org.osgi.framework.BundleContext;
  * @version 0.1
  */
 public class VigenerePlugin extends AbstractUIPlugin {
-	/**
-	 * The plug-in ID
-	 */
-	public static final String PLUGIN_ID = "org.jcryptool.crypto.classic.vigenere"; //$NON-NLS-1$
-
-	/**
-	 * The shared instance
-	 */
-	private static VigenerePlugin plugin;
-	
-	/**
-	 * The constructor
-	 */
-	public VigenerePlugin() {
-		plugin = this;
-	}
-	
-	/**
-	 * The method uses the superclass method which refreshes the plug-in actions.
-	 */
-	@Override
-	public void start(BundleContext context) throws Exception {
-		super.start(context);
-	}
-
-	/**
-	 * The method uses the superclass method which saves this plug-in's 
-	 * preference and dialog stores and shuts down 
-     * its image registry (if they are in use).
-	 */
-	@Override
-	public void stop(BundleContext context) throws Exception {
-		plugin = null;
-		super.stop(context);
-	}
-
-	/**
-	 * Returns the shared instance
-	 *
-	 * @return the shared instance
-	 */
-	public static VigenerePlugin getDefault() {
-		return plugin;
-	}
-
+    /** The plug-in ID. */
+    public static final String PLUGIN_ID = "org.jcryptool.crypto.classic.vigenere"; //$NON-NLS-1$
 }
