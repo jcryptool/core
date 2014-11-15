87951b268df7f8600ef796d9b53a9e6a199c3ea6
Dominik Schadow <dominikschadow@googlemail.com>
2014-11-15 17:24:44 +0100

    Added license information
    Signed-off-by: Dominik Schadow <dominikschadow@googlemail.com>

--- org.jcryptool.visual.merkleHellman/src/org/jcryptool/visual/merkleHellman/MerkleHellmanPlugin.java
+++ org.jcryptool.visual.merkleHellman/src/org/jcryptool/visual/merkleHellman/MerkleHellmanPlugin.java
@@ -1,3 +1,12 @@
+// -----BEGIN DISCLAIMER-----
+/*******************************************************************************
+ * Copyright (c) 2014 JCrypTool Team and Contributors
+ *
+ * All rights reserved. This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+// -----END DISCLAIMER-----
 package org.jcryptool.visual.merkleHellman;
 
 import org.eclipse.jface.resource.ImageDescriptor;
