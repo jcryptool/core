4acc192d8694b0d6c6cd4a155a96e8f83a5368c0
Holger Friedrich <Holger.Friedrich@trivadis.com>
2014-11-26 10:49:28 +0100

    Unified New (Empty|Sample) File for text and hex editor as Commands rather than Actions

---
+++ org.jcryptool.editor.hex/src/org/jcryptool/editor/hex/actions/NewFile.java
@@ -0,0 +1,25 @@
+package org.jcryptool.editor.hex.actions;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.PlatformUI;
+import org.jcryptool.core.logging.utils.LogUtil;
+import org.jcryptool.core.operations.editors.AbstractEditorService;
+import org.jcryptool.editor.hex.HexEditorConstants;
+
+public class NewFile extends AbstractHandler {
+
+	/**
+	 * creates a new window with the sample input of JCrypTool
+	 */
+	public Object execute(ExecutionEvent event) {
+		try {
+			IWorkbenchPage page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
+			page.openEditor(AbstractEditorService.createTemporaryFile(), HexEditorConstants.EditorID);
+		} catch (Exception e) {
+            LogUtil.logError(e);
+		}
+		return null;
+	}
+}
