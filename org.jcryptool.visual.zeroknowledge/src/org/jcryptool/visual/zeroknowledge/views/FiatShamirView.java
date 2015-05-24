da8de53fcd82267e3531895b224b7a0b73f577f5
Dominik Schadow <dominikschadow@googlemail.com>
2015-05-24 13:20:23 +0200

    Organized imports
    Signed-off-by: Dominik Schadow <dominikschadow@googlemail.com>

--- org.jcryptool.visual.zeroknowledge/src/org/jcryptool/visual/zeroknowledge/views/FiatShamirView.java
+++ org.jcryptool.visual.zeroknowledge/src/org/jcryptool/visual/zeroknowledge/views/FiatShamirView.java
@@ -15,17 +15,13 @@ import java.util.Observer;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.ScrolledComposite;
-import org.eclipse.swt.layout.FormLayout;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Group;
-import org.eclipse.swt.widgets.Label;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.part.ViewPart;
-import org.jcryptool.core.util.fonts.FontService;
 import org.jcryptool.visual.zeroknowledge.ModNCalculator;
 import org.jcryptool.visual.zeroknowledge.algorithm.Modell;
 import org.jcryptool.visual.zeroknowledge.algorithm.fiatshamir.FSAlice;
