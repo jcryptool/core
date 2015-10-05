2ab77fb6fe3dd566c051bab3087ba4b3e1cc348d
Dominik Schadow <dominikschadow@googlemail.com>
2015-10-05 11:50:31 +0200

    Removed unused imports
    Signed-off-by: Dominik Schadow <dominikschadow@googlemail.com>

--- org.jcryptool.crypto.classic.model/src/org/jcryptool/crypto/classic/model/algorithm/ClassicAlgorithmConfigurationWithKey.java
+++ org.jcryptool.crypto.classic.model/src/org/jcryptool/crypto/classic/model/algorithm/ClassicAlgorithmConfigurationWithKey.java
@@ -3,7 +3,6 @@ package org.jcryptool.crypto.classic.model.algorithm;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.IEditorPart;
