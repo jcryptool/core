a3f204f9592932b79a32c8651343bca0a8e795d6
Stefan Zuck <s1410239038@students.fh-hagenberg.at>
2016-05-05 16:24:35 +0200

    Added classes for address

---
+++ org.jcryptool.visual.merkletree/src/org/jcryptool/visual/merkletree/algorithm/Address.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package org.jcryptool.visual.merkletree.algorithm;
+
+/**
+ * @author Zuck
+ *
+ *This class implements the address structure to randomize each PRNG call
+ */
+public abstract class Address {
+	byte layerAddress;
+	byte[] treeAddress = new byte[5];
+	byte otsBit;
+	
+	public abstract void setHashAdress(int i);
+	
+	public abstract void setKeyBit(int i);
+	
+	public abstract void setChainAddress(int i);
+	
+	public abstract void setBlockBit(int i);
+	
+	public abstract void setTreeHeight(int i);
+	
+	public abstract void setTreeIndex(int i);
+	
+	public abstract int getTreeHeight();
+	
+	public abstract void setOTSBit(int i);
+	
+	public abstract void setOTSAddress(int i);
+	
+	public abstract void setLTreeBit(int i);
+	
+	public abstract void setLTreeAddress(int i);
+	
+	public abstract void setLayerAddress(int i);
+	
+	public abstract void setTreeAddress(int i);
+
+}
---
+++ org.jcryptool.visual.merkletree/src/org/jcryptool/visual/merkletree/algorithm/OTSHashAddress.java
@@ -0,0 +1,90 @@
+package org.jcryptool.visual.merkletree.algorithm;
+
+public class OTSHashAddress extends Address {
+	byte layerAddress;
+	byte[] treeAddress = new byte[5];
+	byte otsBit;
+	byte[] Address = new byte[3];
+	byte[] chainAddress = new byte[2];
+	byte hashAddress;
+	byte keyBit;
+
+	@Override
+	public void setHashAdress(int i) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setKeyBit(int i) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setChainAddress(int i) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setBlockBit(int i) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setTreeHeight(int i) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setTreeIndex(int i) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int getTreeHeight() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setOTSBit(int i) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setOTSAddress(int i) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setLTreeBit(int i) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setLTreeAddress(int i) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setLayerAddress(int i) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setTreeAddress(int i) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
