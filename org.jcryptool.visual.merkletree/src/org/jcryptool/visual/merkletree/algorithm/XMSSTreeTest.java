b01e897b94f5bedaf9cc418b41be53965ba9e977
christoph <christoph@thinkpad>
2016-05-06 15:39:22 +0200

    added Junit Tests

--- org.jcryptool.visual.merkletree/src/org/jcryptool/visual/merkletree/algorithm/XMSSTreeTest.java
+++ org.jcryptool.visual.merkletree/src/org/jcryptool/visual/merkletree/algorithm/XMSSTreeTest.java
@@ -2,30 +2,27 @@ package org.jcryptool.visual.merkletree.algorithm;
 
 import static org.junit.Assert.*;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 
 import org.junit.Test;
 
 public class XMSSTreeTest {
 
-	@Test
-	public void testXMSSTreeByteArrayByteArrayIntInt() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testXMSSTreeIntInt() {
-		fail("Not yet implemented");
-	}
 
 	@Test
 	public void testAddPrivateSeed() {
-		fail("Not yet implemented");
+		XMSSTree merkle = new XMSSTree(null, null, 0);
+		byte[] privateS = {1, 2, 3, 4};
+		merkle.addPrivateSeed(privateS);
+		assertEquals(merkle.getPrivateSeed(), privateS);	
 	}
 
 	@Test
 	public void testAddPublicSeed() {
-		fail("Not yet implemented");
+		XMSSTree merkle = new XMSSTree(null, null, 0);
+		byte[] publicS = {1, 2, 3, 4};
+		merkle.addPublicSeed(publicS);
+		assertEquals(merkle.getPublicSeed(), publicS);
 	}
 
 	@Test
@@ -35,12 +32,23 @@ public class XMSSTreeTest {
 
 	@Test
 	public void testGetMerkleRoot() {
-		fail("Not yet implemented");
+		XMSSTree merkle = new XMSSTree(null, null, 16);
+		merkle.selectOneTimeSignatureAlgorithm("SHA-256","WOTS");
+		merkle.generateKeyPairsAndLeaves();
+		merkle.generateMerkleTree();
+		byte[] root = merkle.getMerkleRoot();
+		System.out.println(root);
 	}
 
 	@Test
 	public void testGetPrivateSeed() {
-		fail("Not yet implemented");
+		//Generate Merkle Tree
+		XMSSTree merkle = new XMSSTree(null, null, 0);
+		assertEquals(merkle.getPrivateSeed(), null);
+		
+		byte[] privateS = {1, 2, 3, 4, 5 };
+		merkle = new XMSSTree(privateS, null, 0);
+		assertEquals(merkle.getPrivateSeed(), privateS);
 	}
 
 	@Test
@@ -50,17 +58,25 @@ public class XMSSTreeTest {
 
 	@Test
 	public void testGetPublicSeed() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetKeyLength() {
-		fail("Not yet implemented");
+		//Generate Merkle Tree
+		XMSSTree merkle = new XMSSTree(null, null, 0);
+		assertEquals(merkle.getPublicSeed(), null);
+		
+		byte[] publicS = {1, 2, 3, 4, 5 };
+		merkle = new XMSSTree(null, publicS, 0);
+		assertEquals(merkle.getPublicSeed(), publicS);
+		
 	}
 
 	@Test
 	public void testGetLeafCounter() {
-		fail("Not yet implemented");
+		XMSSTree merkle = new XMSSTree(null, null, 0);
+		assertEquals(merkle.getLeafCounter(), 0);
+		
+		merkle = new XMSSTree(null, null, 2);
+		assertEquals(merkle.getLeafCounter(), 2);
+
+
 	}
 
 	@Test
@@ -99,38 +115,25 @@ public class XMSSTreeTest {
 	}
 
 	@Test
-	public void testAppendByteArrays() {
-		fail("Not yet implemented");
-	}
-
-	@Test
 	public void testGetTreeHeight() {
-
-		ArrayList<Node> leaves = new ArrayList<Node>();
-		byte[] content = { 0, 0, 0, 0, 0 };
-		Node leafNode1 = new Node(content, true, 1);
-		Node leafNode2 = new Node(content, true, 2);
-		Node leafNode3 = new Node(content, true, 3);
-		Node leafNode4 = new Node(content, true, 4);
-		Node leafNode5 = new Node(content, true, 5);
-		Node leafNode6 = new Node(content, true, 6);
-		Node leafNode7 = new Node(content, true, 7);
-		Node leafNode8 = new Node(content, true, 8);
-		leaves.add(leafNode1);
-		leaves.add(leafNode2);
-		leaves.add(leafNode3);
-		leaves.add(leafNode4);
-
-		int height = Integer.bitCount(Integer.highestOneBit(leaves.size() - 1) * 2 - 1);
-		assertEquals(height, 2);
 		
-		leaves.add(leafNode5);
-		leaves.add(leafNode6);
-		leaves.add(leafNode7);
-		leaves.add(leafNode8);
+		XMSSTree merkle = new XMSSTree(null, null, 2);
+		merkle.selectOneTimeSignatureAlgorithm("SHA-256","WOTS");
+		merkle.generateKeyPairsAndLeaves();
+		merkle.generateMerkleTree();
+		assertEquals(merkle.getTreeHeight(),1);
 		
-		height = Integer.bitCount(Integer.highestOneBit(leaves.size() - 1) * 2 - 1);
-		assertEquals(height, 3);
+		merkle = new XMSSTree(null, null, 8);
+		merkle.selectOneTimeSignatureAlgorithm("SHA-256","WOTS");
+		merkle.generateKeyPairsAndLeaves();
+		merkle.generateMerkleTree();
+		assertEquals(merkle.getTreeHeight(),3);
+
+		merkle = new XMSSTree(null, null, 16);
+		merkle.selectOneTimeSignatureAlgorithm("SHA-256","WOTS");
+		merkle.generateKeyPairsAndLeaves();
+		merkle.generateMerkleTree();
+		assertEquals(merkle.getTreeHeight(),4);
 	}
 
 	@Test
@@ -159,8 +162,35 @@ public class XMSSTreeTest {
 	}
 
 	@Test
+	public void testGenerateKeyPairsAndLeaves() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsGenerated() {
+		XMSSTree merkle = new XMSSTree(null, null, 0);
+		assertEquals(merkle.isGenerated(), false);
+		merkle.generateMerkleTree();
+		assertEquals(merkle.isGenerated(), true);
+	}
+
+	@Test
 	public void testGetOneTimeSignatureAlgorithm() {
 		fail("Not yet implemented");
 	}
 
+	@Test
+	public void TestrandomGenerator(){
+		XMSSTree merkle = new XMSSTree(null, null, 0);
+		byte[] address = {1,2,3,4};
+		byte[] seed1 = {4,5,6,7,8};
+		byte[] seed2 = {7,5,6,7,8};
+		byte[] psrv1 = merkle.randomGenerator(seed1, address, 20);
+		byte[] psrv2 = merkle.randomGenerator(seed1, address, 20);
+		assertArrayEquals(psrv1, psrv2);
+		
+		psrv2 = merkle.randomGenerator(seed2, address, 20);
+		assertFalse(Arrays.equals(psrv1, psrv2));
+
+	}
 }
