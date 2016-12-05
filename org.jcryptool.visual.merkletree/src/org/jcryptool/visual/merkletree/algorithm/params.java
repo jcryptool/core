9c86058fa093f8b89b91bf69130c8a09e4ecf481
lena_heimberger <S1510239012@students.fh-hagenberg.at>
2016-12-05 15:16:40 +0100

    Made a params class to make code more readable

---
+++ org.jcryptool.visual.merkletree/src/org/jcryptool/visual/merkletree/algorithm/params.java
@@ -0,0 +1,42 @@
+package org.jcryptool.visual.merkletree.algorithm;
+
+/**
+ * putting the XMSS_MT Parameters in a constructor because it makes the code better readable
+ * @author Lena Heimberger
+ *
+ */
+public class params {
+	int d; // layers of the tree
+	int h; // overall tree height
+	int l; // height of a single tree
+	int n;
+	int index_len;
+	
+	public params(int h, int d, int n) throws Exception{
+		if (h % d!=0) {
+		    throw new Exception("Tree height and overall tree height are different- please pick parameters where h%d==0");
+		    //do something fancy?
+		  }
+		  this.h = h;
+		  this.d = d;
+		  this.n = n;
+		  this.index_len = (h + 7) / 8;
+	}
+	
+	public int getH(params P){
+		return this.h;
+	}
+	
+	public int getD(params P){
+		return this.d;
+	}
+	public int getL(params P){
+		return this.l;
+	}
+	public int getN(params P){
+		return this.n;
+	}
+	public int getIndexLength(params P){
+		return this.index_len;
+	}
+}
