9fd1ad2da48139de2400e4487fa9536ad7dc932c
Michael Altenhuber <s1510239014@students.fh-hagenberg.at>
2017-03-16 11:38:02 +0100

    MerkleTreeGeneration: improved variable naming
    	variable names should now be closer to their purpose, making the
    	code more easily understandable

--- org.jcryptool.visual.merkletree/src/org/jcryptool/visual/merkletree/ui/MerkleTreeGeneration.java
+++ org.jcryptool.visual.merkletree/src/org/jcryptool/visual/merkletree/ui/MerkleTreeGeneration.java
@@ -51,44 +51,41 @@ public class MerkleTreeGeneration extends Composite {
 	private ViewPart masterView;
 	private SUIT mode;
 
-	private byte[] seedarray;
-	private byte[] bitmaskSeedarray;
+	private byte[] publicSeed;
+	private byte[] privateSeed;
 
 	private Composite groupMaster;
-	private Button buttonCreateSeed;
-	private Label randomgenerator;
-	private Text textSeed;
-	private int wParameter = 16;
 
-	private int spinnerValue;
+	private Group publicSeedGroup;
+	private Group wParameterGroup;
+	private Group generateKeyGroup;
+	private Group privateSeedGroup;
+
+	private Text publicSeedText;
+	private Button publicSeedButton;
+
+	private Text privateSeedText;
+	private Button privateSeedButton;
+	private StyledText bitmaskDescription;
 
 	// w Parameter Box
 	private Button buttonSet4;
 	private Button buttonSet16;
-	private Label titleLabel;
+	private int wParameter = 16;
 	private StyledText wParamDescription;
 
 	// Generate Keys Box
-	private Button buttonCreateKeys;
-	private Label createLabel;
+	private Button createKeysButton;
 	private Label createdKey;
 	private StyledText generateKeyDescription;
 	private Spinner keypairSpinner;
+	private int spinnerValue;
 
 	private MessageBox successBox;
-	private int allowedIndex;
-	private Label trees;
 	private Combo multiTreeCombo;
 	private int[][] multiTreeArguments;
 	private int multiTreeArgumentIndex;
 	private Label multitreeAmountLabel;
-
-	private Group bitmaskGroup;
-	private Button bitmaskButton;
-	private Label bitmaskLabel;
-	private StyledText bitmaskDescription;
-	private Text bitmaskText;
-	private Label bitmaskRandomgenerator;
 
 	/**
 	 * Create the composite. Including Seed content and KeyPairComposite
@@ -106,45 +103,36 @@ public class MerkleTreeGeneration extends Composite {
 		groupMaster.setLayout(new GridLayout(8, true));
 		groupMaster.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 8, 1));
 
-		/**
-		 * (non-javadoc)
-		 * 
-		 * Seed Box
-		 * 
-		 */
-		Group group = new Group(groupMaster, SWT.NONE);
-		group.setText(Descriptions.Tab0_Head1);
-		group.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 8, 1));
-		group.setLayout(new GridLayout(10, true));
-		group.setFont(FontService.getNormalBoldFont());
+		publicSeedGroup = new Group(groupMaster, SWT.NONE);
+		publicSeedGroup.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 8, 1));
+		publicSeedGroup.setLayout(new GridLayout(10, true));
+		publicSeedGroup.setFont(FontService.getNormalBoldFont());
 
 		/*
 		 * Seed Label
 		 */
-		randomgenerator = new Label(group, SWT.NONE);
-		randomgenerator.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, true, false, 3, 1));
-		// randomgenerator.setText(Descriptions.Tab0_Head1);
+		Label leftSpacer0 = new Label(publicSeedGroup, SWT.NONE);
+		leftSpacer0.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, true, false, 3, 1));
 
 		/*
 		 * Textbox for seed initiates textbox with a seed
 		 */
-		textSeed = new Text(group, SWT.BORDER | SWT.CENTER);
-		textSeed.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 4, 1));
-		seedarray = generateNewSeed();
-		textSeed.setText(Converter._byteToHex(seedarray));
+		publicSeedText = new Text(publicSeedGroup, SWT.BORDER | SWT.CENTER);
+		publicSeedText.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 4, 1));
+		publicSeed = generateNewSeed();
+		publicSeedText.setText(Converter._byteToHex(publicSeed));
 
 		/*
 		 * Button generate new Seed
 		 */
-		buttonCreateSeed = new Button(group, SWT.NONE);
-		buttonCreateSeed.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, true, false, 3, 1));
-		buttonCreateSeed.setText(Descriptions.Tab0_Button0);
+		publicSeedButton = new Button(publicSeedGroup, SWT.NONE);
+		publicSeedButton.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, true, false, 3, 1));
 
-		buttonCreateSeed.addSelectionListener(new SelectionAdapter() {
+		publicSeedButton.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
-				seedarray = generateNewSeed();
-				textSeed.setText(Converter._byteToHex(seedarray));
+				publicSeed = generateNewSeed();
+				publicSeedText.setText(Converter._byteToHex(publicSeed));
 				((MerkleTreeView) masterView).updateElement();
 			}
 		});
@@ -161,47 +149,44 @@ public class MerkleTreeGeneration extends Composite {
 		 * 
 		 */
 		if (mode == SUIT.XMSS || mode == SUIT.XMSS_MT) {
-			bitmaskGroup = new Group(groupMaster, SWT.NONE);
-			bitmaskGroup.setText(Descriptions.Tab0_Head5);
-			bitmaskGroup.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 8, 1));
-			bitmaskGroup.setLayout(new GridLayout(10, true));
-			bitmaskGroup.setText(Descriptions.Tab0_Head3);
-			bitmaskGroup.setFont(FontService.getNormalBoldFont());
+			privateSeedGroup = new Group(groupMaster, SWT.NONE);
+			privateSeedGroup.setText(Descriptions.Tab0_Head5);
+			privateSeedGroup.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 8, 1));
+			privateSeedGroup.setLayout(new GridLayout(10, true));
+			privateSeedGroup.setText(Descriptions.Tab0_Seed0);
+			privateSeedGroup.setFont(FontService.getNormalBoldFont());
 
-			bitmaskDescription = new StyledText(bitmaskGroup, SWT.WRAP | SWT.BORDER);
-			bitmaskDescription.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false, 10, 2));
-			bitmaskDescription.setText(Descriptions.Tab0_Txt3);
-			bitmaskDescription.setEditable(false);
-			bitmaskDescription.setCaret(null);
+			// bitmaskDescription = new StyledText(bitmaskGroup, SWT.WRAP |
+			// SWT.BORDER);
+			// bitmaskDescription.setLayoutData(new GridData(SWT.FILL, SWT.FILL,
+			// true, false, 10, 2));
+			// bitmaskDescription.setText(Descriptions.Tab0_Txt3);
+			// bitmaskDescription.setEditable(false);
+			// bitmaskDescription.setCaret(null);
 
-			/*
-			 * Bitmask Seed Label
-			 */
-			bitmaskRandomgenerator = new Label(bitmaskGroup, SWT.NONE);
-			bitmaskRandomgenerator.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, true, false, 3, 1));
-			bitmaskRandomgenerator.setText(Descriptions.Tab0_Head4);
-
+			Label leftSpacer1 = new Label(privateSeedGroup, SWT.NONE);
+			leftSpacer1.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, true, false, 3, 1));
 			/*
 			 * Textbox for seed
 			 */
-			bitmaskText = new Text(bitmaskGroup, SWT.BORDER | SWT.CENTER);
-			bitmaskText.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 4, 1));
+			privateSeedText = new Text(privateSeedGroup, SWT.BORDER | SWT.CENTER);
+			privateSeedText.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 4, 1));
 
 			/*
 			 * Button generate new Seed
 			 */
-			bitmaskButton = new Button(bitmaskGroup, SWT.NONE);
-			bitmaskButton.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, true, false, 3, 1));
-			bitmaskButton.setText(Descriptions.Tab0_Button1);
+			privateSeedButton = new Button(privateSeedGroup, SWT.NONE);
+			privateSeedButton.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, true, false, 3, 1));
+			privateSeedButton.setText(Descriptions.Tab0_Seed3);
 
-			bitmaskSeedarray = generateNewSeed();
-			bitmaskText.setText(Converter._byteToHex(bitmaskSeedarray));
+			privateSeed = generateNewSeed();
+			privateSeedText.setText(Converter._byteToHex(privateSeed));
 
-			bitmaskButton.addSelectionListener(new SelectionAdapter() {
+			privateSeedButton.addSelectionListener(new SelectionAdapter() {
 				@Override
 				public void widgetSelected(SelectionEvent e) {
-					bitmaskSeedarray = generateNewSeed();
-					bitmaskText.setText(Converter._byteToHex(bitmaskSeedarray));
+					privateSeed = generateNewSeed();
+					privateSeedText.setText(Converter._byteToHex(privateSeed));
 					((MerkleTreeView) masterView).updateElement();
 				}
 			});
@@ -214,7 +199,7 @@ public class MerkleTreeGeneration extends Composite {
 		 * 
 		 */
 
-		Group wParameterGroup = new Group(groupMaster, SWT.NONE);
+		wParameterGroup = new Group(groupMaster, SWT.NONE);
 		wParameterGroup.setText(Descriptions.Tab0_Head5);
 		wParameterGroup.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 8, 1));
 		GridLayout wParameterGroupLayout = new GridLayout(8, true);
@@ -272,7 +257,7 @@ public class MerkleTreeGeneration extends Composite {
 		 * 
 		 */
 
-		Group generateKeyGroup = new Group(groupMaster, SWT.NONE);
+		generateKeyGroup = new Group(groupMaster, SWT.NONE);
 		generateKeyGroup.setText(Descriptions.Tab0_Head2);
 		generateKeyGroup.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 8, 1));
 		GridLayout generateKeyGroupLayout = new GridLayout(1, true);
@@ -343,9 +328,9 @@ public class MerkleTreeGeneration extends Composite {
 		createdKey.setText(Descriptions.MerkleTreeKey_1);
 
 		// 'create button'
-		buttonCreateKeys = new Button(keyRow, SWT.NONE);
-		buttonCreateKeys.setLayoutData(new GridData(SWT.RIGHT, SWT.FILL, true, false, 5, 2));
-		buttonCreateKeys.setFont(FontService.getNormalBoldFont());
+		createKeysButton = new Button(keyRow, SWT.NONE);
+		createKeysButton.setLayoutData(new GridData(SWT.RIGHT, SWT.FILL, true, false, 5, 2));
+		createKeysButton.setFont(FontService.getNormalBoldFont());
 
 		if (mode == SUIT.XMSS_MT) {
 			Label keysum = new Label(keyRow, SWT.NONE);
@@ -372,41 +357,37 @@ public class MerkleTreeGeneration extends Composite {
 			});
 		}
 
-		// setting the text's depending on the actual suite
-
-		// createLabel.setText(Descriptions.Tab0_Head2);
-
 		// MessageBox when successfully creating a Key
 		successBox = new MessageBox(this.getShell(), SWT.ICON_WORKING | SWT.OK);
 		successBox.setMessage(Descriptions.MerkleTreeKey_3);
 
 		switch (mode) {
 		case XMSS:
-			generateKeyDescription.setText(Descriptions.XMSS.Tab0_Txt2);
+			publicSeedGroup.setText(Descriptions.Tab0_Seed1);
+			publicSeedButton.setText(Descriptions.Tab0_Seed2);
 			wParamDescription.setText(Descriptions.XMSS.Tab0_Txt1);
-			buttonCreateKeys.setText(Descriptions.XMSS.Tab0_Key_Button);
+			generateKeyDescription.setText(Descriptions.XMSS.Tab0_Txt2);
+			createKeysButton.setText(Descriptions.XMSS.Tab0_Key_Button);
 			successBox.setText(Descriptions.XMSS.Tab0_MessageBox0);
+
 			break;
 		case XMSS_MT:
-			generateKeyDescription.setText(Descriptions.XMSS_MT.Tab0_Txt2);
+			publicSeedGroup.setText(Descriptions.Tab0_Seed1);
+			publicSeedButton.setText(Descriptions.Tab0_Seed2);
 			wParamDescription.setText(Descriptions.XMSS_MT.Tab0_Txt1);
-			group.setText(Descriptions.XMSS_MT.Tab0_Seed0);
-			bitmaskGroup.setText(Descriptions.XMSS_MT.Tab0_Seed1);
-			bitmaskButton.setText(Descriptions.XMSS_MT.Tab0_Seed2);
-			buttonCreateSeed.setText(Descriptions.XMSS_MT.Tab0_Seed3);
-			bitmaskRandomgenerator.setText("");
-			bitmaskDescription.dispose();
-			buttonCreateKeys.setText(Descriptions.XMSS_MT.Tab0_Key_Button);
+			generateKeyDescription.setText(Descriptions.XMSS_MT.Tab0_Txt2);
+			createKeysButton.setText(Descriptions.XMSS_MT.Tab0_Key_Button);
 			successBox.setText(Descriptions.XMSS_MT.Tab0_MessageBox0);
-
-			this.layout();
 			break;
 		case MSS:
 		default:
-			generateKeyDescription.setText(Descriptions.MSS.Tab0_Txt2);
+			publicSeedGroup.setText(Descriptions.Tab0_Head1);
+			publicSeedButton.setText(Descriptions.Tab0_Button0);
 			wParamDescription.setText(Descriptions.MSS.Tab0_Txt1);
-			buttonCreateKeys.setText(Descriptions.MSS.Tab0_Key_Button);
+			generateKeyDescription.setText(Descriptions.MSS.Tab0_Txt2);
+			createKeysButton.setText(Descriptions.MSS.Tab0_Key_Button);
 			successBox.setText(Descriptions.MSS.Tab0_MessageBox0);
+
 			break;
 		}
 
@@ -414,7 +395,7 @@ public class MerkleTreeGeneration extends Composite {
 		 * Event Listener for the generate keys button if this button is pressed
 		 * a new merkle tree is generated
 		 */
-		buttonCreateKeys.addSelectionListener(new SelectionAdapter() {
+		createKeysButton.addSelectionListener(new SelectionAdapter() {
 			/*
 			 * (non-Javadoc) generates the MerkleTree and the KeyPairs
 			 */
@@ -476,16 +457,16 @@ public class MerkleTreeGeneration extends Composite {
 				// if the generated Tree is a XMSSTree -> the
 				// bitmaskseed is also needed
 				if (mode == SUIT.XMSS) {
-					((XMSSTree) merkle).setBitmaskSeed(bitmaskSeedarray);
+					((XMSSTree) merkle).setBitmaskSeed(privateSeed);
 				}
 				if (mode == SUIT.XMSS_MT) {
 					merkle.setLeafCount(multiTreeArguments[multiTreeArgumentIndex][1]);
 					((MultiTree) merkle).setSingleTreeHeight(multiTreeArguments[multiTreeArgumentIndex][0]);
-					((MultiTree) merkle).setSKSeed(seedarray);
-					((MultiTree) merkle).setPKSeed(bitmaskSeedarray);
+					((MultiTree) merkle).setSKSeed(publicSeed);
+					((MultiTree) merkle).setPKSeed(privateSeed);
 				} else {
 					merkle.setLeafCount(spinnerValue);
-					merkle.setSeed(seedarray);
+					merkle.setSeed(publicSeed);
 				}
 
 				// merkle.setLeafCount(spinnerValue);
