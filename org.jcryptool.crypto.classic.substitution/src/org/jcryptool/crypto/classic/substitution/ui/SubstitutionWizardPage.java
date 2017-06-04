3366acc3bcfd76339e315137d6eb5a7b54b7b01f
Dominik Schadow <dominikschadow@gmail.com>
2017-06-04 21:46:29 +0200

    Imports cleanup

--- org.jcryptool.crypto.classic.substitution/src/org/jcryptool/crypto/classic/substitution/ui/SubstitutionWizardPage.java
+++ org.jcryptool.crypto.classic.substitution/src/org/jcryptool/crypto/classic/substitution/ui/SubstitutionWizardPage.java
@@ -20,8 +20,6 @@ import java.util.Set;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Shell;
