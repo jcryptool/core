ed90a6d488eb1f1b07deedc8769dae89d8c4224f
Thorben <Thorben@Thorben-PC.fritz.box>
2017-10-13 10:28:41 +0200

    GUI changed new Button to directly open an editor, if none is open

--- org.jcryptool.analysis.entropy/src/org/jcryptool/analysis/entropy/ui/EntropyUI.java
+++ org.jcryptool.analysis.entropy/src/org/jcryptool/analysis/entropy/ui/EntropyUI.java
@@ -12,25 +12,27 @@ package org.jcryptool.analysis.entropy.ui;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.CTabFolder;
 import org.eclipse.swt.custom.CTabItem;
-import org.eclipse.swt.graphics.Point;
-import org.eclipse.swt.graphics.Rectangle;
-import org.eclipse.swt.layout.FillLayout;
+//import org.eclipse.swt.graphics.Point;
+//import org.eclipse.swt.graphics.Rectangle;
+//import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.FormAttachment;
 import org.eclipse.swt.layout.FormData;
 import org.eclipse.swt.layout.FormLayout;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Shell;
+//import org.eclipse.swt.widgets.Display;
+//import org.eclipse.swt.widgets.Shell;
 import org.jcryptool.core.logging.utils.LogUtil;
 
 /**
- * This code was edited or generated using CloudGarden's Jigloo SWT/Swing GUI Builder, which is free
- * for non-commercial use. If Jigloo is being used commercially (ie, by a corporation, company or
- * business for any purpose whatever) then you should purchase a license for each developer using
- * Jigloo. Please visit www.cloudgarden.com for details. Use of Jigloo implies acceptance of these
- * licensing terms. A COMMERCIAL LICENSE HAS NOT BEEN PURCHASED FOR THIS MACHINE, SO JIGLOO OR THIS
- * CODE CANNOT BE USED LEGALLY FOR ANY CORPORATE OR COMMERCIAL PURPOSE.
+ * This code was edited or generated using CloudGarden's Jigloo SWT/Swing GUI
+ * Builder, which is free for non-commercial use. If Jigloo is being used
+ * commercially (ie, by a corporation, company or business for any purpose
+ * whatever) then you should purchase a license for each developer using Jigloo.
+ * Please visit www.cloudgarden.com for details. Use of Jigloo implies
+ * acceptance of these licensing terms. A COMMERCIAL LICENSE HAS NOT BEEN
+ * PURCHASED FOR THIS MACHINE, SO JIGLOO OR THIS CODE CANNOT BE USED LEGALLY FOR
+ * ANY CORPORATE OR COMMERCIAL PURPOSE.
  */
 public class EntropyUI extends Composite {
 	private CTabFolder cMainTabFolder;
@@ -42,7 +44,8 @@ public class EntropyUI extends Composite {
 	private EntropyUItable compositeTable;
 
 	/**
-	 * Overriding checkSubclass allows this class to extend org.eclipse.swt.widgets.Composite
+	 * Overriding checkSubclass allows this class to extend
+	 * org.eclipse.swt.widgets.Composite
 	 */
 	protected void checkSubclass() {
 	}
@@ -51,29 +54,30 @@ public class EntropyUI extends Composite {
 		return cMainTabFolder;
 	}
 
-	/**
-	 * Auto-generated method to display this org.eclipse.swt.widgets.Composite inside a new Shell.
-	 */
-	public static void showGUI() {
-		Display display = Display.getDefault();
-		Shell shell = new Shell(display);
-		EntropyUI inst = new EntropyUI(shell, SWT.NULL);
-		Point size = inst.getSize();
-		shell.setLayout(new FillLayout());
-		shell.layout();
-		if (size.x == 0 && size.y == 0) {
-			inst.pack();
-			shell.pack();
-		} else {
-			Rectangle shellBounds = shell.computeTrim(0, 0, size.x, size.y);
-			shell.setSize(shellBounds.width, shellBounds.height);
-		}
-		shell.open();
-		while (!shell.isDisposed()) {
-			if (!display.readAndDispatch())
-				display.sleep();
-		}
-	}
+	// /**
+	// * Auto-generated method to display this org.eclipse.swt.widgets.Composite
+	// inside a new Shell.
+	// */
+	// public static void showGUI() {
+	// Display display = Display.getDefault();
+	// Shell shell = new Shell(display);
+	// EntropyUI inst = new EntropyUI(shell, SWT.NULL);
+	// Point size = inst.getSize();
+	// shell.setLayout(new FillLayout());
+	// shell.layout();
+	// if (size.x == 0 && size.y == 0) {
+	// inst.pack();
+	// shell.pack();
+	// } else {
+	// Rectangle shellBounds = shell.computeTrim(0, 0, size.x, size.y);
+	// shell.setSize(shellBounds.width, shellBounds.height);
+	// }
+	// shell.open();
+	// while (!shell.isDisposed()) {
+	// if (!display.readAndDispatch())
+	// display.sleep();
+	// }
+	// }
 
 	public EntropyUI(org.eclipse.swt.widgets.Composite parent, int style) {
 		super(parent, style);
@@ -86,8 +90,6 @@ public class EntropyUI extends Composite {
 			{
 				cMainTabFolder = new CTabFolder(this, SWT.NONE);
 				FormData cMainTabFolderLData = new FormData();
-				cMainTabFolderLData.width = 622;
-				cMainTabFolderLData.height = 288;
 				cMainTabFolderLData.left = new FormAttachment(0, 1000, 0);
 				cMainTabFolderLData.top = new FormAttachment(0, 1000, 0);
 				cMainTabFolderLData.bottom = new FormAttachment(1000, 1000, 0);
