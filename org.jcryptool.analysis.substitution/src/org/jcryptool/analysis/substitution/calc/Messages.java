1655c73ebfad17ab3166fff3246cecc77dd3805b
BrunoVernay <BrunoVern.a@gmail.com>
2018-03-24 23:37:26 +0100

    Add French to the Substitution Analysis

--- org.jcryptool.analysis.substitution/src/org/jcryptool/analysis/substitution/calc/Messages.java
+++ org.jcryptool.analysis.substitution/src/org/jcryptool/analysis/substitution/calc/Messages.java
@@ -16,6 +16,7 @@ public class Messages extends NLS {
 	private static final String BUNDLE_NAME = "org.jcryptool.analysis.substitution.calc.messages"; //$NON-NLS-1$
 	public static String DynamicPredefinedStatisticsProvider_3;
 	public static String DynamicPredefinedStatisticsProvider_5;
+	public static String DynamicPredefinedStatisticsProvider_6;
 	static {
 		// initialize resource bundle
 		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
