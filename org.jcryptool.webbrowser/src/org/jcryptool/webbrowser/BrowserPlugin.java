ea20abc011cddfa0a36b4bf5430efb406fb0da96
Thorben Groos <thorben.groos@student.uni-siegen.de>
2018-06-01 18:29:41 +0200

    Added an empty line just to trigger the release build on 02.06.2018

--- org.jcryptool.webbrowser/src/org/jcryptool/webbrowser/BrowserPlugin.java
+++ org.jcryptool.webbrowser/src/org/jcryptool/webbrowser/BrowserPlugin.java
@@ -16,6 +16,7 @@ import org.eclipse.ui.plugin.AbstractUIPlugin;
  * The activator class controls the plug-in life cycle
  */
 public class BrowserPlugin extends AbstractUIPlugin {
+	
     /** The plug-in ID. */
     public static final String PLUGIN_ID = "org.jcryptool.webbrowser"; //$NON-NLS-1$
 
