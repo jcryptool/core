01865225c36d9ee559b8861ba9399ef45b2250b3
Thorben <thorben.groos@student.uni-siegen.de>
2018-09-19 22:35:10 +0200

    Added YELLOW

--- org.jcryptool.core.util/src/org/jcryptool/core/util/colors/ColorService.java
+++ org.jcryptool.core.util/src/org/jcryptool/core/util/colors/ColorService.java
@@ -23,7 +23,7 @@ import org.eclipse.swt.widgets.Display;
  */
 public class ColorService {
     /** Colors for backgrounds. */
-    public static final Color WHITE, GREEN, RED, LIGHTGRAY, GRAY, BLACK;
+    public static final Color WHITE, GREEN, RED, LIGHTGRAY, GRAY, BLACK, YELLOW;
 
     static {
         WHITE = Display.getDefault().getSystemColor(SWT.COLOR_WHITE);
@@ -32,5 +32,6 @@ public class ColorService {
         GREEN = Display.getDefault().getSystemColor(SWT.COLOR_GREEN);
         LIGHTGRAY = Display.getDefault().getSystemColor(SWT.COLOR_WIDGET_BACKGROUND);
         BLACK = Display.getDefault().getSystemColor(SWT.COLOR_BLACK);
+        YELLOW = Display.getDefault().getSystemColor(SWT.COLOR_YELLOW);
     }
 }
