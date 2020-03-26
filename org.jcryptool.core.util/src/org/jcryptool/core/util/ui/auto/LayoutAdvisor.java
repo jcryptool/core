44800c6696fc7b65172cbd56ba518e5ec8d25fc8
Simon Leischnig <simon-jena@gmail.com>
2020-03-26 18:48:23 +0100

    org/jcryptool/core/util/ui/auto/LayoutAdvisor.java: more conservative

--- org.jcryptool.core.util/src/org/jcryptool/core/util/ui/auto/LayoutAdvisor.java
+++ org.jcryptool.core.util/src/org/jcryptool/core/util/ui/auto/LayoutAdvisor.java
@@ -7,9 +7,11 @@ import java.util.function.Predicate;
 import org.eclipse.jface.viewers.CellEditor.LayoutData;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.ScrolledComposite;
+import org.eclipse.swt.custom.StyledText;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Text;
 
 public class LayoutAdvisor {
 	
@@ -68,17 +70,21 @@ public class LayoutAdvisor {
 				traverseMarkWidthHint_FirstStrategy(child);
 			}
 		} else {
+			boolean hasWrap = (c.getStyle() & SWT.WRAP) != 0;
+// 			System.err.println(String.format("%s has wrap: %s", c, hasWrap));
+			if (hasWrap) {
 				Object layoutData = c.getLayoutData();
 				if (layoutData instanceof GridData) {
 					GridData gridData = (GridData) layoutData;
 // 					System.err.println(String.format("%s has GridData: %s and widthHint: %s, at a SWT.DEFAULT of %s", c, gridData, gridData.widthHint, SWT.DEFAULT));
-					if(gridData.widthHint == SWT.DEFAULT) {
+					if(gridData.widthHint == SWT.DEFAULT || c instanceof Text || c instanceof StyledText) {
 						List<Composite> parents = getParents(c);
 // 						System.err.println(String.format("%s has GridData: %s and parents: %s", c, gridData, parents));
 						if (parents.stream().anyMatch(parent -> parent.getClass().equals(ScrolledComposite.class))) {
 							// here, the layout is changed globally
 							// conditions:
 							// * is below a ScrolledComposite
+							// * has the SWT.WRAP style or is a Text or StyledText widget 
 							// * has a GridLayout object 
 							int computedSize = c.computeSize(SWT.DEFAULT, SWT.DEFAULT).x;
 							gridData.widthHint = computedSize;
@@ -87,6 +93,8 @@ public class LayoutAdvisor {
 					}
 				}
 			}
+			
+		}
 		
 	}
 	
