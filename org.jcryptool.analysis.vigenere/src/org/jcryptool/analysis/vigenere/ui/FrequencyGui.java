3623a04155327fb445eac6d587e56f9c8a8a680a
Thorben Groos <thorben.groos@student.uni-siegen.de>
2020-04-05 13:02:29 +0200

    Fixes a problem that the graphs width increases after resizing the window.

--- org.jcryptool.analysis.vigenere/src/org/jcryptool/analysis/vigenere/ui/FrequencyGui.java
+++ org.jcryptool.analysis.vigenere/src/org/jcryptool/analysis/vigenere/ui/FrequencyGui.java
@@ -138,14 +138,17 @@ public class FrequencyGui extends Content {
 			cgraph = new Composite(mainComposite, SWT.NONE);
 			cgraph.setLayout(new GridLayout());
 			GridData gd_cgraph = new GridData(SWT.FILL, SWT.FILL, true, true, 2, 1);
+			gd_cgraph.widthHint = 600;
 			gd_cgraph.minimumHeight = 250;
 			cgraph.setLayoutData(gd_cgraph);
 
 			thelp = new Text(mainComposite, SWT.MULTI | SWT.WRAP);
-			thelp.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false, 1, 2));
 			thelp.setText(String.format(Messages.FrequencyGui_text_help, plength) + Messages.FrequencyGui_0);
 			thelp.setFont(FontService.getSmallFont());
 			thelp.setEditable(false);
+			GridData gd_thelp = new GridData(SWT.FILL, SWT.FILL, true, false, 1, 2);
+			gd_thelp.widthHint = 300;
+			thelp.setLayoutData(gd_thelp);
 
 			llength = new Label(mainComposite, SWT.NONE);
 			llength.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false));
