c8945f0175a28b52ad3b91e8e1b4d01cc80c5ed0
Simon Leischnig <simon-jena@gmx.net>
2021-03-09 15:44:19 +0100

    #236 RCP restart use RELAUNCH and cmdline- and ini-filters; defunct
    Reson for defunct: seems the eclipse.exitdata property should be
    a fully fledged commandline; but Platform.getCommandLineArgs()
    is not sufficient here.

--- org.jcryptool.core/src/org/jcryptool/core/Application.java
+++ org.jcryptool.core/src/org/jcryptool/core/Application.java
@@ -17,15 +17,19 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Locale;
 import java.util.Properties;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.equinox.app.IApplication;
 import org.eclipse.equinox.app.IApplicationContext;
 import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.osgi.service.environment.EnvironmentInfo;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.MessageBox;
@@ -43,9 +47,26 @@ import org.jcryptool.core.logging.utils.LogUtil;
  * @author Dominik Schadow
  * @version 1.0.0
  */
+/**
+ * @author snuc
+ *
+ */
+/**
+ * @author snuc
+ *
+ */
 public class Application implements IApplication {
 
-    /*
+	/**
+	 * If this field is set to something else than null, the application will consider 
+	 * the exit code {@link IApplication#EXIT_RESTART} as {@link IApplication#EXIT_RELAUNCH}
+	 * and set the exitdata property accordingly. This is e.g. used to filter out language 
+	 * switches a.k.a. `-nl {language}`. Since other important switches like `-data {workspace}` may
+	 * be present, we can't throw out every command line parameter — hence this filtering approach.
+	 */
+    private static Function<List<String>, List<String>> restart_cmdlinefilter = null;
+
+	/*
      * (non-Javadoc)
      * @see org.eclipse.equinox.app.IApplication#start(org.eclipse.equinox.app.IApplicationContext)
      */
@@ -73,28 +94,25 @@ public class Application implements IApplication {
 					shell = new Shell();
 				}
 				LanguageChooser chooser = new LanguageChooser(shell);
-				int retcode = chooser.open();
-				restartWithChangedLanguage(chooser.nl, false);
+				int retcode = chooser.open(); // actually, we have to restart, so we ignore the return code. Also, there is no cancel button.
+
+				Function<List<String>, List<String>> inifileFilter = createLanguageRewriteInifileFilter(chooser.nl);
+				applyFilterToInifile(inifileFilter);
+
+				// this filters the cmdline arguments so that they don't change the language back
+				Function<List<String>, List<String>> cmdlineFilter = createCmdlineRewriteRemoveLanguagespecFilter();
+				filterCommandlineArgsForRelaunch(context, cmdlineFilter);
+				// this is a low-level restart (can't use workbench here)
+				return IApplication.EXIT_RELAUNCH;
         	}
         } catch (Exception e) {
             LogUtil.logError(CorePlugin.PLUGIN_ID, e);
         }
         
-        Display display = PlatformUI.createDisplay();
-        try {
-            int returnCode = PlatformUI.createAndRunWorkbench(display, new ApplicationWorkbenchAdvisor());
-            if (returnCode == PlatformUI.RETURN_RESTART) {
-                return IApplication.EXIT_RESTART;
-            } else {
-                return IApplication.EXIT_OK;
-            }
-        } finally {
-            display.dispose();
-        }
-
+        return startApplication(context);
     }
-
-    /*
+    
+	/*
      * (non-Javadoc)
      * @see org.eclipse.equinox.app.IApplication#stop()
      */
@@ -116,14 +134,80 @@ public class Application implements IApplication {
     }
 
 
+
     /**
-     * Sets the language in the <b>JCrypTool.ini</b>. This file is located in a different folder on Mac OS X.
-     * 
-     * @param language
-     * @throws IOException 
-     * @throws Exception
+     * Starts the GUI and controls the application's return code behavior.
+     * @param context 
+     * @return the exit code; see e.g. {@link IApplication#EXIT_RELAUNCH}
      */
-    private static void setLanguage(String language) throws IOException {
+    private Object startApplication(IApplicationContext context) {
+        Display display = PlatformUI.createDisplay();
+        // the application return code behavior
+        try {
+            int returnCode = PlatformUI.createAndRunWorkbench(display, new ApplicationWorkbenchAdvisor());
+            if (returnCode == PlatformUI.RETURN_RESTART) {
+            	if (restart_cmdlinefilter != null) {
+            		filterCommandlineArgsForRelaunch(context, restart_cmdlinefilter);
+            		return IApplication.EXIT_RELAUNCH;
+				} else {
+					return IApplication.EXIT_RESTART;
+				}
+            } else {
+                return IApplication.EXIT_OK;
+            }
+        } finally {
+            display.dispose();
+        }
+	}
+
+    /**
+     * Filters commandlineargs for a relaunch. Only applicable within the {@link #start(IApplicationContext)} or {@link #startApplication(IApplicationContext)} functions.
+     * If this behavior is desired elsewhere, see {@link Application#restart_cmdlinefilter} and use Workbench.restart().
+     * 
+     * @param context the application context
+     * @param filter the filter to use on the commandline args
+     * @return
+     */
+    private static void filterCommandlineArgsForRelaunch(IApplicationContext context, Function<List<String>, List<String>> filter) {
+    	// TODO: these do not include the launcher or the equinox runnables!! Find `EnvironmentInfo` seems the way to proceed.
+    	List<String> args = Arrays.asList(Platform.getCommandLineArgs());
+    	List<String> filteredArgs = filter.apply(args);
+
+		// TODO: escape whitespace? e.g. for -data ... paths?
+    	String resultargs = filteredArgs.stream().collect(Collectors.joining(" "));
+		// TODO: test if that actually has the desired effect
+    	System.setProperty("eclipse.exitdata", resultargs);
+	}
+
+    /**
+     * This restarts the application. May only be called when the workbench has been started yet! Else, 
+     * see the code pertaining to {@link Application#restart_cmdlinefilter} and {@link #startApplication(IApplicationContext)}
+     * 
+     * @param ask whether a dialog box should be opened that asks whether the action should be performed.
+     * @param cmdlineFilter a filter that is applied to the commandline arguments
+     */
+	private static void restartAppWithCommandlinefilter(boolean ask, Function<List<String>, List<String>> cmdlineFilter) {
+		boolean doRestart = false;
+		if (ask) {
+			MessageBox mbox = new MessageBox(Display.getDefault().getActiveShell(), SWT.ICON_QUESTION | SWT.YES | SWT.NO);
+			mbox.setText(org.jcryptool.core.preferences.pages.Messages.MessageTitleRestart);
+			mbox.setMessage(org.jcryptool.core.preferences.pages.Messages.MessageRestart);
+			if (mbox.open() == SWT.YES) {
+				LogUtil.logInfo(org.jcryptool.core.preferences.pages.Messages.MessageLogRestart);
+				doRestart = true;
+			}
+		} else {
+			doRestart = true;
+		}
+		
+		if (doRestart) {
+			// this instructs the application to filter commandline arguments and relaunching instead of restarting
+			Application.restart_cmdlinefilter = cmdlineFilter;
+			PlatformUI.getWorkbench().restart();
+		}
+    }
+    
+	private static void applyFilterToInifile(Function<List<String>, List<String>> filter) throws IOException {
         String path = Platform.getInstallLocation().getURL().toExternalForm();
         String fileNameOrg = Platform.getProduct().getName() + ".ini"; //$NON-NLS-1$
         String fileNameBak = fileNameOrg + ".bak"; //$NON-NLS-1$
@@ -143,54 +227,80 @@ public class Application implements IApplication {
         fileOrg.renameTo(fileBak);
         BufferedReader in = new BufferedReader(new FileReader(fileBak));
         BufferedWriter out = new BufferedWriter(new FileWriter(fileOrg));
-        var lines = in.lines().collect(Collectors.toList());
-        in.close();
-        var linesOut = new LinkedList<String>();
-        boolean previousWasNLFlag = false;
-        for(String line: lines) {
-        	boolean isNlFlag = line.trim().equals("-nl");
-        	// first, keep all lines that don't have to do with language
-        	if (isNlFlag || previousWasNLFlag) {
-				linesOut.add(line); // TODO: includes linebreak?
-			}
-        	// keep track of state
-			if (isNlFlag) {
-        		previousWasNLFlag = true;
-        	} else {
-        		previousWasNLFlag = false;
-			}
-        }
-        linesOut.add("-nl");
-        linesOut.add(language);
+
+        // the filtering step
+        List<String> lines = in.lines().collect(Collectors.toList());
+		var linesOut = filter.apply(lines);
+
 		for(String outLine : linesOut) {
         	out.write(outLine + "\n");
         }
+
 		out.flush();
 		out.close();
-        
-    }
-
-    private static void restartAppWithInifileReload(boolean ask) {
-		boolean doRestart = false;
-		if (ask) {
-			MessageBox mbox = new MessageBox(Display.getDefault().getActiveShell(), SWT.ICON_QUESTION | SWT.YES | SWT.NO);
-			mbox.setText(org.jcryptool.core.preferences.pages.Messages.MessageTitleRestart);
-			mbox.setMessage(org.jcryptool.core.preferences.pages.Messages.MessageRestart);
-			if (mbox.open() == SWT.YES) {
-				LogUtil.logInfo(org.jcryptool.core.preferences.pages.Messages.MessageLogRestart);
-				doRestart = true;
-			}
-		} else {
-			doRestart = true;
-		}
-		
-		if (doRestart) {
-			PlatformUI.getWorkbench().restart();
-		}
-    }
-    
-	public static void restartWithChangedLanguage(String language, boolean ask) throws IOException {
-		setLanguage(language);
-		restartAppWithInifileReload(ask);
+        in.close();
 	}
+	
+	/**
+	 * rewrites a commandline so that it does not include -nl flags
+	 * @param newLanguage the new language (e.g. "en")
+	 * @return the new commandline as list
+	 */
+	private static Function<List<String>, List<String>> createLanguageRewriteInifileFilter(String newLanguage) {
+		return (List<String> in) -> {
+			var linesOut = new LinkedList<String>();
+			boolean previousWasNLFlag = false;
+			for(String line: in) {
+				boolean isNlFlag = line.trim().equals("-nl");
+				// first, keep all lines that don't have to do with language
+				if (! (isNlFlag || previousWasNLFlag)) {
+					linesOut.add(line);
+				}
+				// keep track of state
+				if (isNlFlag) {
+					previousWasNLFlag = true;
+				} else {
+					previousWasNLFlag = false;
+				}
+			}
+			// add the new language flags at the end
+			linesOut.add("-nl");
+			linesOut.add(newLanguage);
+			return linesOut;
+		};
+	}
+
+	/**
+	 * rewrites a commandline so that it does not include -nl flags
+	 * 
+	 * @return the new commandline as list
+	 */
+	private static Function<List<String>, List<String>> createCmdlineRewriteRemoveLanguagespecFilter() {
+		return (List<String> in) -> {
+			var linesOut = new LinkedList<String>();
+			boolean previousWasNLFlag = false;
+			for(String line: in) {
+				boolean isNlFlag = line.trim().equals("-nl");
+				// first, keep all lines that don't have to do with language
+				if (! (isNlFlag || previousWasNLFlag)) {
+					linesOut.add(line);
+				}
+				// keep track of state
+				if (isNlFlag) {
+					previousWasNLFlag = true;
+				} else {
+					previousWasNLFlag = false;
+				}
+			}
+			return linesOut;
+		};
+	}
+	
+	public static void restartWithChangedLanguage(String language, boolean ask) throws IOException {
+		Function<List<String>, List<String>> cmdlineFilter = createCmdlineRewriteRemoveLanguagespecFilter();
+		Function<List<String>, List<String>> inifileFilter = createLanguageRewriteInifileFilter(language);
+		applyFilterToInifile(inifileFilter);
+		restartAppWithCommandlinefilter(ask, cmdlineFilter);
+	}
+
 }
