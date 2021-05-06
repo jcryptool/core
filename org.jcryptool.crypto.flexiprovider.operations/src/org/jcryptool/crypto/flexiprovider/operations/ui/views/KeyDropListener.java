02d26c35c94308184ffe0d12cadb8cc1f6c988f3
Thorben Groos <thorben.groos@web.de>
2021-05-06 12:20:25 +0200

    Updated disclaimer to 2021

--- org.jcryptool.crypto.flexiprovider.operations/src/org/jcryptool/crypto/flexiprovider/operations/ui/views/KeyDropListener.java
+++ org.jcryptool.crypto.flexiprovider.operations/src/org/jcryptool/crypto/flexiprovider/operations/ui/views/KeyDropListener.java
@@ -1,6 +1,6 @@
 // -----BEGIN DISCLAIMER-----
 /*******************************************************************************
- * Copyright (c) 2011, 2020 JCrypTool Team and Contributors
+ * Copyright (c) 2011, 2021 JCrypTool Team and Contributors
  * 
  * All rights reserved. This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License v1.0 which
