ca1b9edc37f1e57b2fcabafa4e44baf8e705df6e
Simon Leischnig <simon-jena@gmx.net>
2021-05-21 15:59:25 +0200

    Hotfix JCT keystore behavior (no persistence, hint in keystore view)

--- org.jcryptool.crypto.keystore/src/org/jcryptool/crypto/keystore/backend/KeyStoreManager.java
+++ org.jcryptool.crypto.keystore/src/org/jcryptool/crypto/keystore/backend/KeyStoreManager.java
@@ -218,6 +218,7 @@ public class KeyStoreManager {
      * keystores content.
      */
     private void saveKeystore() {
+    	if(1 == 1) return; // disabled since loading the keystore does not work. Removed as of 2021-05-21 by simlei. original implementation below.
         OutputStream os = null;
         try {
         	ProviderManager2.getInstance().pushFlexiProviderPromotion();
