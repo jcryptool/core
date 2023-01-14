beb05d0426da0e65ddced0f40c98ffb6fbf372ac
Simon Leischnig <simon-jena@gmx.net>
2023-01-14 18:06:14 +1100

    temporarily disable notification check

--- org.jcryptool.core.help/src/org/jcryptool/core/help/ServerStartup.java
+++ org.jcryptool.core.help/src/org/jcryptool/core/help/ServerStartup.java
@@ -1,11 +1,17 @@
 package org.jcryptool.core.help;
 
+import java.io.IOException;
 import java.net.URI;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
+import java.util.Scanner;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 import org.eclipse.core.commands.Command;
 import org.eclipse.core.commands.CommandManager;
@@ -14,7 +20,13 @@ import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.commands.NotHandledException;
 import org.eclipse.core.commands.common.NotDefinedException;
 import org.eclipse.core.runtime.Platform;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.browser.Browser;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IStartup;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.help.IWorkbenchHelpSystem;
@@ -56,12 +68,103 @@ public class ServerStartup implements IStartup {
 		
 		
 	}
+	public static String readStringFromURL(String requestURL) throws IOException
+	{
+	    try (Scanner scanner = new Scanner(new URL(requestURL).openStream(),
+	            StandardCharsets.UTF_8.toString()))
+	    {
+	        scanner.useDelimiter("\\A");
+	        return scanner.hasNext() ? scanner.next() : "";
+	    }
+	}	
+	public List<String> getIdsForVersion(String version) {
+		var url = "https://cryptool.org/jct/notifications/" + version + ".ids";
+		try {
+			System.out.println("DBG: Checking for new notifications from " + url);
+			var idscontent = readStringFromURL(url);
+			List<String> lines = idscontent.lines().filter(l -> ! l.isBlank()).collect(Collectors.toList());
+			lines.forEach(l -> System.out.println("DBG: read id " + l));
+			return lines;
+		} catch (IOException e) {
+			System.out.println("Error in getting ids from: " + url);
+			return new LinkedList<String>();
+		}
+	}
+	public String getURLForId(String id) {
+		return "https://cryptool.org/jct/notifications/messages/" + id + ".html";
+	}
+	public String getHTMLForId(String id) {
+		try {
+			var content = readStringFromURL(getURLForId(id));
+			return content;
+		} catch (IOException e) {
+			return "<div>Failed to fetch message from " + "https://cryptool.org/jct/notifications/messages/" + id + ".html" + "</div>";
+		}
+	}
 	
+	public void notificationChecker() {
+// 		var version = System.getProperty("")
+		var version = "Weekly-Build--1.0.8.24-20221010";
+		var idsForVersion = getIdsForVersion(version);
+		var idsForAll = getIdsForVersion("all");
+		List<String> seenMessages = new LinkedList<String>();
+		List<String> unseenMessagesBeforeFilter = new LinkedList<String>();
+		for(String versionId : idsForVersion) { unseenMessagesBeforeFilter.add(versionId); }
+		for(String allId : idsForAll) { unseenMessagesBeforeFilter.add(allId); }
+		List<String> unseenMessages = unseenMessagesBeforeFilter.stream().filter(m -> ! seenMessages.contains(m)).distinct().collect(Collectors.toList());
+//		var unseenMessageContents = unseenMessagesBeforeFilter.stream().map(id -> getHTMLForId(id)).collect(Collectors.toList());
+		System.out.println("DBG: unseen: " + unseenMessages.size());
+		if (unseenMessages.size() > 0) {
+			var delayed = new Thread() {
+				public void run() {
+					try {
+						Thread.sleep(5000);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+					Display.getDefault().asyncExec(new Runnable() {
+						public void run() {
+							var url = getURLForId(unseenMessages.get(0));
+							System.out.println("DBG: opening shell: " + url);
+							var display = Display.getDefault();
+							var shell = new Shell(display);
+							shell.setLayout(new GridLayout());
+							var comp = new Composite(shell, SWT.NONE);
+							comp.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+							comp.setLayout(new GridLayout());
+							var viewer = new Browser(comp, SWT.NONE);
+							viewer.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+							viewer.setUrl(url);
+//							var w = display.getBounds().width / 2;
+//							var h = display.getBounds().height / 2;
+//							var x = display.getBounds().width / 4;
+//							var y = display.getBounds().height / 4;
+//							shell.setBounds(x, y, w, h);
+							shell.setSize(800,600);
+							shell.setText("A notification for users of JCrypTool version " + version);
+							shell.layout();
+// 							shell.pack();
+							shell.open();
+							while (!shell.isDisposed()) {
+								if (! display.isDisposed()) {
+							        if (!display.readAndDispatch()) {
+										display.sleep();
+							        }
+								}
+							}
+						}
+					});
+				}
+			};
+			delayed.run();
+		}
+		
+	}
 	@Override
 	public void earlyStartup() {
-    System.out.println("Hello World 1");
 		StartupParsed helpAtStartupParsed = StartupParsed.parse();
 
+// 		notificationChecker(); // TODO: reactivate
 		if (helpAtStartupParsed.activated) {
 			Display display = PlatformUI.getWorkbench().getWorkbenchWindows()[0].getShell().getDisplay();
 			display.syncExec(new Runnable() {
@@ -78,6 +181,8 @@ public class ServerStartup implements IStartup {
 		} else {
 			return;
 		}
+		
+		
 
 //		Display display = PlatformUI.getWorkbench().getWorkbenchWindows()[0].getShell().getDisplay();
 // 		oldStartup();
